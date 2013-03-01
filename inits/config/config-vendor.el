91515e157ced6149ba4bd254cb12c2107f1a73b2
mytoh <mytoh@gmail.com>
2013-03-02 07:11:05 +0900

    add navi2ch and helm-ag

--- inits/config/config-vendor.el
+++ inits/config/config-vendor.el
@@ -5,7 +5,7 @@
 
 ;;; plugins under vendor directory
 ;; github repositories
-(defun *my-vendor-packages*
+(defun *my-vendor-packages* ()
   '(("emacs-powerline" "jonathanchu/emacs-powerline")
     ("nyan-mode" "TeMPOraL/nyan-mode")
     ("highlight-sexp" "daimrod/highlight-sexp")
@@ -15,24 +15,27 @@
     ("seijiseikana-el" "moriyamahiroshi/seijiseikana-el")
     ("highlight-cl" "emacsmirror/highlight-cl")
     ("eldoc-extension" "emacsmirror/eldoc-extension")
+    ("navi2ch" "naota/navi2ch")
+    ("helm-ag" "syohex/emacs-helm-ag")
     ;; ("emacs-evernote-mode" "http://emacs-evernote-mode.google.com/svn/trunk")
     ))
 
 
 ;; install packages
-;; (my-vendor-install-packages
-;;  (*my-vendor-packages*))
+(my-vendor-install-packages
+ (*my-vendor-packages*))
 
 
 ;; update plugins
 ;;(my-vendor-update-packages *user-emacs-vendor-directory*)
 
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "nyan-mode"))
+;; add to load-path
+(add-subdirs-to-load-path *user-emacs-vendor-directory*)
+
+
 ;; (nyan-mode t)
 
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "emux-el"))
+
 (req 'emux
      ;; prefix C-c
      ;; "C-n" ' emux:term-next
@@ -46,9 +49,6 @@
      )
 
 ;; highlight-sexp
-(my-add-to-load-path
- (concat-path *user-emacs-vendor-directory*
-              "highlight-sexp"))
 ;; (req 'highlight-sexp
 ;;     (add-hook 'lisp-mode-hook 'highlight-sexp-mode)
 ;;   (add-hook 'emacs-lisp-mode-hook 'highlight-sexp-mode)
@@ -57,26 +57,18 @@
 ;;   (setq hl-sexp-background-color "#1a1a1a"))
 
 ;; auto-highlight-symbol-mode
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "auto-highlight-symbol-mode"))
 (req 'auto-highlight-symbol
      (global-auto-highlight-symbol-mode t))
 
 ;; highlight-cl
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "highlight-cl"))
 (req 'highlight-cl
      (add-hook 'emacs-lisp-mode-hook 'highlight-cl-add-font-lock-keywords)
      (add-hook 'lisp-interaction-mode-hook 'highlight-cl-add-font-lock-keywords))
 
 ;; stumpwm
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "stumpwm-mode"))
 (req 'stumpwm-mode)
 
 ;; helm-delicious
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "helm-delicious"))
 (req 'helm-delicious
      (req 'auth-source
           (if (file-exists-p "~/.authinfo.gpg")
@@ -84,13 +76,18 @@
             (setq auth-sources '((:source "~/.authinfo" :host t :protocol t))))))
 
 ;; seijiseikana
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "seijiseikana-el"))
 (req 'seijiseikana)
 
 ;; eldoc-extension
-(my-add-to-load-path (concat-path *user-emacs-vendor-directory*
-                                  "eldoc-extension"))
 (req 'eldoc-extension)
+
+;; navi2ch
+(req 'navi2ch
+     ;; use mona font
+     (setq navi2ch-mona-enable 1))
+
+;; helm-ag
+(req 'helm-ag
+     )
 
 (provide 'config-vendor)
