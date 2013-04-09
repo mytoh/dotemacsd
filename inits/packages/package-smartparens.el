6dca12e35f28dfb2567b62d74cea9b6a10ea2f55
mytoh <mytoh1114@gmail.com>
2013-04-10 08:11:14 +0900

    * inits/packages/package-smartparens.el: change keymap

--- inits/packages/package-smartparens.el
+++ inits/packages/package-smartparens.el
@@ -10,46 +10,34 @@
 
      ;;;;;;;;;;;;
      ;; keys
-     (defvar my-sp-paredit-bindings '(
-                                      ("C-M-f" . sp-forward-sexp) ;; navigation
-                                      ("C-M-b" . sp-backward-sexp)
-                                      ("C-M-u" . sp-backward-up-sexp)
-                                      ("C-M-d" . sp-down-sexp)
-                                      ("C-M-p" . sp-backward-down-sexp)
-                                      ("C-M-n" . sp-up-sexp)
-                                      ("M-s" . sp-splice-sexp) ;; depth-changing commands
-                                      ("M-<up>" . sp-splice-sexp-killing-backward)
-                                      ("M-<down>" . sp-splice-sexp-killing-forward)
-                                      ("M-r" . sp-splice-sexp-killing-around)
-                                      ("C-)" . sp-forward-slurp-sexp) ;; barf/slurp
-                                      ("C-<right>" . sp-forward-slurp-sexp)
-                                      ("C-}" . sp-forward-barf-sexp)
-                                      ("C-<left>" . sp-forward-barf-sexp)
-                                      ("C-(" . sp-backward-slurp-sexp)
-                                      ("C-M-<left>" . sp-backward-slurp-sexp)
-                                      ("C-{" . sp-backward-barf-sexp)
-                                      ("C-M-<right>" . sp-backward-barf-sexp)
-                                      ("M-S" . sp-split-sexp) ;; misc
 
-                                      ;; added
-                                      ("M-f" . sp-forward-symbol)
-                                      ("M-b" . sp-backward-symbol)
-                                      ("C-M-k" . sp-kill-sexp)
-                                      ("C-]" . sp-select-next-thing-exchange)
-                                      ("C-M-]" . sp-select-next-thing))
-       "Alist containing the default paredit bindings to corresponding
-smartparens functions.")
+     (define-key sp-keymap (kbd "C-M-f")       'sp-forward-sexp) ;; navigation
+     (define-key sp-keymap (kbd "C-M-b")       'sp-backward-sexp)
+     (define-key sp-keymap (kbd "C-M-u")       'sp-backward-up-sexp)
+     (define-key sp-keymap (kbd "C-M-d")       'sp-down-sexp)
+     (define-key sp-keymap (kbd "C-M-p")       'sp-backward-down-sexp)
+     (define-key sp-keymap (kbd "C-M-n")       'sp-up-sexp)
+     (define-key sp-keymap (kbd "M-s")         'sp-splice-sexp) ;; depth-changing commands
+     (define-key sp-keymap (kbd "M-<up>")      'sp-splice-sexp-killing-backward)
+     (define-key sp-keymap (kbd "M-<down>")    'sp-splice-sexp-killing-forward)
+     (define-key sp-keymap (kbd "M-r")         'sp-splice-sexp-killing-around)
+     (define-key sp-keymap (kbd "C-)")         'sp-forward-slurp-sexp) ;; barf/slurp
+     (define-key sp-keymap (kbd "C-<right>")   'sp-forward-slurp-sexp)
+     (define-key sp-keymap (kbd "C-}")         'sp-forward-barf-sexp)
+     (define-key sp-keymap (kbd "C-<left>")    'sp-forward-barf-sexp)
+     (define-key sp-keymap (kbd "C-(")         'sp-backward-slurp-sexp)
+     (define-key sp-keymap (kbd "C-M-<left>")  'sp-backward-slurp-sexp)
+     (define-key sp-keymap (kbd "C-{")         'sp-backward-barf-sexp)
+     (define-key sp-keymap (kbd "C-M-<right>") 'sp-backward-barf-sexp)
+     (define-key sp-keymap (kbd "M-S")         'sp-split-sexp) ;; misc
 
-     (req 'dash
-          (defun my-sp-use-paredit-bindings ()
-            "Initiate `sp-keymap' with paredit-compatible bindings for
-corresponding functions provided by smartparens.  See variable
-`sp-paredit-bindings'."
-            (interactive)
-            (--each my-sp-paredit-bindings
-              (define-key sp-keymap (read-kbd-macro (car it)) (cdr it))))
+     ;; added
+     (define-key sp-keymap (kbd "M-f")   'sp-forward-symbol)
+     (define-key sp-keymap (kbd "M-b")   'sp-backward-symbol)
+     (define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp)
+     (define-key sp-keymap (kbd "C-]")   'sp-select-next-thing-exchange)
+     (define-key sp-keymap (kbd "C-M-]") 'sp-select-next-thing)
 
-          (my-sp-use-paredit-bindings))
 
      ;; from wiki/Tips-and-tricks
      (defun my-sp-wrap-with-paren (&optional arg)
@@ -59,8 +47,7 @@ corresponding functions provided by smartparens.  See variable
      (define-key sp-keymap (kbd "M-(") 'my-sp-wrap-with-paren)
 
      ;; lisp modes
-     (sp-with-modes '(
-                      emacs-lisp-mode
+     (sp-with-modes '(emacs-lisp-mode
                       inferior-emacs-lisp-mode
                       lisp-interaction-mode
                       scheme-mode
