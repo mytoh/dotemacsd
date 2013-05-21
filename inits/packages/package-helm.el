20936f9000e7a1bb9f233ed531ceb0d97f12dfda
mytoh <mytoh@gmail.com>
2013-05-21 19:08:17 +0900

    * inits/packages/package-helm.el: add key binding

--- inits/packages/package-helm.el
+++ inits/packages/package-helm.el
@@ -4,6 +4,7 @@
      (req 'helm-command)
      (global-set-key (kbd "C-c h") 'helm-mini)
      (global-set-key (kbd "M-x") 'helm-M-x)
+     (global-set-key (kbd "C-c C-m") 'helm-M-x)
      (global-set-key (kbd "C-x C-f") 'helm-find-files)
      (global-set-key (kbd "M-y") 'helm-show-kill-ring)
      (global-set-key (kbd "C-M-z") 'helm-resume)
