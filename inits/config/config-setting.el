2435b3dd1e6fbfcf5dc571834be305acd5ccea01
mytoh <mytoh@gmail.com>
2013-08-10 09:07:29 +0900

    update

--- inits/config/config-setting.el
+++ inits/config/config-setting.el
@@ -1,123 +1,123 @@
 ;;; -*- coding: utf-8 -*-
-;; personal initialize
+;;;; personal initialize
 
-;; user elisps
+;;;; user elisps
 (my-add-to-load-path (concat user-emacs-directory "elisp"))
 
 ;;disable startup message
 (enable-option inhibit-startup-screen)
 
 
-;;; parens
+;;;; parens
 (setq show-paren-delay 0
       show-parne-style 'parenthesis)
 (show-paren-mode)
 
-;; syntax highlight
+;;;; syntax highlight
 (global-font-lock-mode 1)
-;;; dont enable this option
+;;;; dont enable this option
 ;; (enable-option font-lock-maximum-decoration)
 
-;; read symlinked file
+;;;; read symlinked file
 (enable-option vc-follow-symlinks)
 
-;; encodings
+;;;; encodings
 (prefer-coding-system 'utf-8-unix)
 
-;; start server
+;;;; start server
 (req 'server
-     (enable-option server-use-tcp)
+     ;; (enable-option server-use-tcp)
      (unless (server-running-p)
        (server-start)))
 
-;; use space instead of tab
+;;;; use space instead of tab
 (setq-default tab-width 4
               indent-tabs-mode nil)
 (set-option indent-line-function 'indent-relative-maybe)
 
-;; mouse
+;;;; mouse
 (set-option mouse-avoidance-mode 'banish)
 
-;; show info on mode-line
+;;;; show info on mode-line
 (enable-option display-time-24hr-format)
 (enable-option display-time-day-and-date)
 (display-time)
 (line-number-mode 1)
 (column-number-mode 1)
 
-;; change yes-no to y-n
+;;;; change yes-no to y-n
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; show imagesnabnab
+;;;; show imagesnabnab
 (auto-image-file-mode 1)
 
-;; highlight region
+;;;; highlight region
 (transient-mark-mode 1)
 
-;; highlight current line
+;;;; highlight current line
 (enable-option global-hl-line-mode)
 
-;; display keys
+;;;; display keys
 (set-option echo-keystrokes 0.1)
 
-;; disable dialog box
+;;;; disable dialog box
 (set-option use-dialog-box nil)
 
-;; smooth scrolling
+;;;; smooth scrolling
 ;; (setq redisplay-dont-pause 1
 ;;       scroll-margin 1
 ;;       scroll-step 4
 ;;       scroll-conservatively 35
 ;;       scroll-preserve-screen-position 1)
 
-;;; disable bars
+;;;; disable bars
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;;; delete whole line with C-k once
+;;;; delete whole line with C-k once
 (enable-option kill-whole-line)
 
-;; enable rectangular mode
+;;;; enable rectangular mode
 (cua-mode 1)
 (disable-option cua-enable-cua-keys)
 
-;;; ignore case
+;;;; ignore case
 (enable-option completion-ignore-case)
 (enable-option read-file-name-completion-ignore-case)
 
-;; alaways show completions
+;;;; alaways show completions
 ;; (icomplete-mode 1)
 
-;; show function name
+;;;; show function name
 (which-function-mode 1)
 (set-face-attribute 'which-func nil :foreground "#4f7b8a")
 
-;; enable lexical binding
+;;;; enable lexical binding
 (enable-option lexical-binding)
 
-;; save buffer history
+;;;; save buffer history
 (savehist-mode 1)
 (set-option history-length 100)
 
-;; disable bell
+;;;; disable bell
 (set-option ring-bell-function 'ignore)
 (enable-option visible-bell)
 
-;; no warnings when compile
+;;;; no warnings when compile
 (set-option byte-compile-warnings '(not cl-functions))
 
-;; dont split verticaly
+;;;; dont split verticaly
 (disable-option split-height-threshold)
 (disable-option split-width-threshold)
 
-;;; backup and autosave
+;;;; backup and autosave
 ;; disable backup
 (enable-option backup-inhibited)
 
-;; disable autosave
+;;;; disable autosave
 (disable-option auto-save-default)
-;; delete auto save file when exit
+;;;; delete auto save file when exit
 (enable-option delete-auto-save-files)
 
 ;; (setq auto-save-file-name-transforms
@@ -127,40 +127,40 @@
 
 ;; save more recent files
 (set-option recentf-max-saved-items 1000)
-;; recentf exclude
+;;;; recentf exclude
 (set-option recentf-exclude `(,(rx  ".el.gz" string-end) "archive-contents$"))
-;; don't record symbolic link file name
+;;;; don't record symbolic link file name
 (enable-option find-file-visit-truename)
 
-;; undo
+;;;; undo
 (set-option undo-limit 100000)
 (set-option undo-string-limit 1300000)
 
-;; reload buffer
+;;;; reload buffer
 (global-auto-revert-mode 1)
 (set-option auto-revert-interval 5)
 (enable-option auto-revert-check-vc-info)
 
-;; confirmation when visit new file or buffer
+;;;; confirmation when visit new file or buffer
 (set-option confirm-nonexistent-file-or-buffer nil)
 
-;; open archived files
+;;;; open archived files
 (auto-compression-mode 1)
 
-;; also reload dired, and quiet
+;;;; also reload dired, and quiet
 (enable-option global-auto-revert-non-file-buffers)
 (disable-option auto-revert-verbose)
 
-;; display buffer end
+;;;; display buffer end
 (enable-option indicate-empty-lines)
 
-;; electric indent
+;;;; electric indent
 (enable-option electric-layout-mode)
 
-;; save minibuffer history
+;;;; save minibuffer history
 (savehist-mode 1)
 
-;; add newline to end of file
+;;;; add newline to end of file
 ;; (enable-option require-final-newline)
 
 
