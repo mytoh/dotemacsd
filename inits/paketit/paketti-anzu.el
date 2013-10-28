6dc801f7a28f10fecffd21265cfde7f6a07b043a
mytoh <mytoh@gmail.com>
2013-10-28 17:06:28 +0900

    add anzu keymap

--- inits/paketit/paketti-anzu.el
+++ inits/paketit/paketti-anzu.el
@@ -1,6 +1,8 @@
 
 (req 'anzu
      (global-anzu-mode +1)
+     (global-set-key (kbd "M-%") 'anzu-query-replace)
+     (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
      ;; (enable-option anzu-use-migemo)
      )
 
