59a113689382f6feed5502d4868a4e678be7bdc5
mytoh <mytoh@gmail.com>
2013-10-28 17:06:59 +0900

    change colour of tabbar

--- inits/paketit/paketti-tabbar.el
+++ inits/paketit/paketti-tabbar.el
@@ -4,6 +4,7 @@
      ;; http://idita.blog11.fc2.com/blog-entry-810.html
      (setq tabbar-cycle-scope 'tabs)
      (setq tabbar-use-images nil)
+     (setq tabbar-separator '(1.0)) ;; space between tabs
      (tabbar-mode t)
      ;; disbale buttons no left side
      (dolist (btn '(tabbar-home-button
@@ -15,15 +16,15 @@
      ;; face
      (set-face-attribute 'tabbar-default nil
                          :background (face-background 'default)
-                         )
+                         :height 1.0)
 
      (set-face-attribute 'tabbar-selected nil
-                         :foreground "#c36d68"
-                         :background (face-background 'default)
+                         :foreground "#ffffff"
+                         :background "#336bcb"
                          :box nil)
      (set-face-attribute 'tabbar-unselected nil
-                         :foreground "#b0efa0"
-                         :background (face-background 'default)
+                         :foreground "#7b7b8b"
+                         :background "#424242"
                          :box '(:line-width 1 :color "gray14" :style nil))
      (set-face-attribute 'tabbar-button nil
                          :background (face-background 'default)
@@ -32,7 +33,8 @@
                          :background (face-background 'default)
                          :height 60)
      ;; firefox keybind
-     (global-set-key [(control tab)] 'tabbar-forward-tab)
-     (global-set-key [(control shift tab)] 'tabbar-backward-tab))
+     (global-set-key (kbd "M-]") 'tabbar-forward)  ; 次のタブ
+     (global-set-key (kbd "M-[") 'tabbar-backward) ; 前のタブ
+     )
 
 (provide 'paketti-tabbar)
