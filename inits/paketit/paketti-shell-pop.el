acb576eb1fa2168099b2f266a2167dcdb3274139
mytoh <mytoh@gmail.com>
2013-11-05 14:06:20 +0900

    fix keymap

--- inits/paketit/paketti-shell-pop.el
+++ inits/paketit/paketti-shell-pop.el
@@ -1,6 +1,6 @@
 (req 'shell-pop
      ;; (setq 'shell-pop-shell-type "eshell")
-     (setq 'shell-pop-window-position "bottom")
-     (setq 'shell-pop-universal-key "C-z"))
+     (setq shell-pop-window-position "bottom")
+     (setq shell-pop-universal-key "C-z"))
 
 (provide 'paketti-shell-pop)
