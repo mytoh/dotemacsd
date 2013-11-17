3b77a81537ce4de1cf173f74894d1465ff729df1
mytoh <mytoh@gmail.com>
2013-11-17 22:16:32 +0900

    add company-mode

---
+++ inits/paketit/paketti-company-mode.el
@@ -0,0 +1,26 @@
+
+
+(req 'company
+
+     (define-key company-active-map (kbd "C-n") 'company-select-next)
+     (define-key company-active-map (kbd "C-p") 'company-select-previous)
+
+
+     (set-face-attribute 'company-tooltip-selection nil
+                         :background "#ab4223")
+
+     (set-face-attribute 'company-tooltip nil
+                         :foreground "#dfdfe1"
+                         :background "#393939"
+                         )
+
+     (set-face-attribute 'company-tooltip-common nil
+                         :foreground "LightSkyBlue")
+
+     (set-face-attribute 'company-tooltip-common-selection nil
+                         :foreground "LightSkyBlue")
+
+     (add-hook 'after-init-hook #'global-company-mode)
+     )
+
+(provide 'paketti-company-mode)
