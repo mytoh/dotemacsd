12cab6a2e5909c83ea09f1324cb517fcc8d1ac89
mytoh <mytoh@gmail.com>
2013-12-04 11:56:46 +0900

    add org-plug-contrib setting file

---
+++ inits/paketit/paketti-org-plus-contrib.el
@@ -0,0 +1,12 @@
+
+(req 'org-indent
+
+     (cl-defun my-org-add-before-save-hook
+         ;; add-hook hook function &optional append local
+         (add-hook 'before-save-hook 'org-indent-indent-buffer nil t))
+
+     (add-hook 'org-mode-hook #'my-org-add-before-save-hook)
+
+     )
+
+(provide 'paketti-org-plus-contrib)
