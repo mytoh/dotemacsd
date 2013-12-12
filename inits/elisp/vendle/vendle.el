20f187b53731a8835cee33d6791a33aa758295c6
mytoh <mytoh@gmail.com>
2013-12-12 17:22:12 +0900

    vendle: change install and update functions for register-local

--- inits/elisp/vendle/vendle.el
+++ inits/elisp/vendle/vendle.el
@@ -66,48 +66,44 @@
 
 ;;;; update
 
-(cl-defun vendle:update-packages (path)
-  (when (file-exists-p path)
-    (cl-letf ((paths (cl-mapcar
-                      #'(lambda (p) (vendle:concat-path path (vendle:package-name p)))
-                      *vendle-package-list*)))
-      (cl-mapc
-       #'vendle:update-package
-       paths))))
+(cl-defun vendle:update-packages ()
+  (when (file-exists-p *user-emacs-vendle-directory*)
+    (cl-mapc
+     #'vendle:update-package
+     *vendle-package-list*)))
 
-(cl-defun vendle:update-package (path)
-  (when (and (vendle:directory-git-p path)
-             (not (file-symlink-p path)))
-    (progn
-      (cd-absolute path)
-      (message "updating vendle package %s.." path)
-      (shell-command "git pull")
-      (cd-absolute user-emacs-directory)
-      (byte-recompile-directory path 0)
-      (message "updating vendle package %s..done" path))))
+(cl-defun vendle:update-package (package)
+  (cl-letf ((path (vendle:package-path package)))
+    (when (and (cl-equalp 'git (vendle:package-type package))
+               (not (file-symlink-p path)))
+      (progn
+        (cd-absolute path)
+        (message "updating vendle package %s.." path)
+        (shell-command "git pull")
+        (cd-absolute user-emacs-directory)
+        (byte-recompile-directory path 0)
+        (message "updating vendle package %s..done" path)))))
 
 ;;;; install
 
-(cl-defun vendle:install-packages (path)
+(cl-defun vendle:install-packages ()
   (cl-mapc
    (lambda (package)
-     (vendle:install-package package path))
+     (vendle:install-package package))
    *vendle-package-list*))
 
-(cl-defun vendle:install-package (package path)
-  (if (not (file-exists-p path))
-      (make-directory path))
-  (unless (file-exists-p (vendle:concat-path path (vendle:package-path package)))
+(cl-defun vendle:install-package (package)
+  (unless (or (cl-equalp 'local (vendle:package-type package))
+              (file-exists-p (vendle:package-path package)))
     (cond ((cl-equalp 'git (vendle:package-type package))
-           (vendle:install-package-git package path)))))
+           (vendle:install-package-git package)))))
 
-(cl-defun vendle:install-package-git (package path)
-  (cd-absolute path)
+(cl-defun vendle:install-package-git (package)
   (message "installing plugin %s" (vendle:package-name package))
   (shell-command (concat  "git clone " (vendle:package-url package) " "
-                          (vendle:package-name package))
-                 path)
-  (byte-recompile-directory (vendle:concat-path path (vendle:package-path package)) 0))
+                          (vendle:package-path package))
+                 *user-emacs-vendle-directory*)
+  (byte-recompile-directory (vendle:package-path package)  0))
 
 
 ;;;; register
