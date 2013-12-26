50b8190262f7758cf81f40e980d2509e9bec27da
mytoh <mytoh@gmail.com>
2013-12-27 08:17:59 +0900

    srfi.el: use cl-lib functions

--- inits/elisp/srfi/srfi.el
+++ inits/elisp/srfi/srfi.el
@@ -4,22 +4,23 @@
 
 
 (cl-defun srfi:add-keywords (face-name keyword-rules)
-  (let* ((keyword-list (mapcar #'(lambda (x)
-                                   (symbol-name (cdr x)))
-                               keyword-rules))
-         (keyword-regexp (concat "(\\("
-                                 (regexp-opt keyword-list)
-                                 "\\)[ \n]")))
-    (my-log "adding keywords for face "
-            (propertize (symbol-name face-name) 'face 'font-lock-variable-name-face)
-            " on scheme mode")
-    (font-lock-add-keywords 'scheme-mode
-                            `((,keyword-regexp 1 ',face-name))))
-  (mapc #'(lambda (x)
-            (put (cdr x)
-                 'scheme-indent-function
-                 (car x)))
-        keyword-rules))
+  (cl-let* ((keyword-list (cl-mapcar #'(lambda (x)
+                                         (symbol-name (cdr x)))
+                                     keyword-rules))
+            (keyword-regexp (cl-concatenate 'string
+                                            "(\\("
+                                            (regexp-opt keyword-list)
+                                            "\\)[ \n]")))
+           (my-log "adding keywords for face "
+                   (propertize (symbol-name face-name) 'face 'font-lock-variable-name-face)
+                   " on scheme mode")
+           (font-lock-add-keywords 'scheme-mode
+                                   `((,keyword-regexp 1 ',face-name))))
+  (cl-mapc #'(lambda (x)
+               (put (cdr x)
+                    'scheme-indent-function
+                    (car x)))
+           keyword-rules))
 
 
 (require 'srfi-1)
