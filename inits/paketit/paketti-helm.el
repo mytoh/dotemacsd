b3175d5e5658806112159bcaba4eb11d866aacae
mytoh <mytoh@gmail.com>
2014-01-03 01:40:03 +0900

    paketti-helm: add face

--- inits/paketit/paketti-helm.el
+++ inits/paketit/paketti-helm.el
@@ -53,6 +53,8 @@
      (set-face-attribute 'helm-source-header nil
                          :background "#324262")
 
+     (set-face-attribute 'helm-selection nil
+                         :background "#337755")
 
      )
 
