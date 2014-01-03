9017b4d7abb5a68805206c3446ad5fed3f0c0819
mytoh <mytoh@gmail.com>
2014-01-03 15:24:52 +0900

    paketti-helm-recoll.el: add helm-recoll setting

---
+++ inits/paketit/paketti-helm-recoll.el
@@ -0,0 +1,12 @@
+;;; paketti-helm-recoll.el -*- lexical-binding: t -*-
+
+(req 'helm-recoll
+     (when (executable-find "recoll")
+       (helm-recoll-create-source "main" "~/.recoll"))
+     )
+
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
