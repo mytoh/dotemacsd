c6fa12c751c42e0257afadfb72fe2f695b78a538
mytoh <mytoh@gmail.com>
2014-01-07 08:36:30 +0900

    paketti-guide-key.el: add org-mode

--- inits/paketit/paketti-guide-key.el
+++ inits/paketit/paketti-guide-key.el
@@ -1,7 +1,8 @@
 
 (req 'guide-key
      (setq guide-key/guide-key-sequence
-           '("C-x r" "C-x 4" "C-x c"))
+           '("C-x r" "C-x 4" "C-x c"
+             (org-mode "C-c C-x")))
      (guide-key-mode 1))
 
 (provide 'paketti-guide-key)
