507a64668d317cb317554713235314471069bade
mytoh <mytoh@gmail.com>
2014-01-07 08:37:00 +0900

    paketti-git-gutter.el: add disable mode

--- inits/paketit/paketti-git-gutter.el
+++ inits/paketit/paketti-git-gutter.el
@@ -4,6 +4,7 @@
      (setq git-gutter:modified-sign "☁")
      (setq git-gutter:added-sign "☀")
      (setq git-gutter:deleted-sign "☂")
+     (setq git-gutter:disabled-modes '(image-mode))
      (global-git-gutter-mode t)
      )
 
