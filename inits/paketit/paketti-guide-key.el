85f7714504eff7522ea39a007e52fce50e08397b
mytoh <mytoh@gmail.com>
2014-01-07 08:45:30 +0900

    paketti-guide-key.el: add helm-command-prefix-key check

--- inits/paketit/paketti-guide-key.el
+++ inits/paketit/paketti-guide-key.el
@@ -1,7 +1,10 @@
 
 (req 'guide-key
      (setq guide-key/guide-key-sequence
-           '("C-x r" "C-x 4" "C-x c"
+           `("C-x r" "C-x 4"
+             ,(if (boundp 'helm-command-prefix-key)
+                  helm-command-prefix-key
+                "C-x c")
              (org-mode "C-c C-x")))
      (guide-key-mode 1))
 
