0ec7b6228ede6683bef99be578854d6408311a4f
mytoh <mytoh@gmail.com>
2014-01-11 04:01:16 +0900

    paketti-helm-package.el: add file for helm-package

---
+++ inits/paketit/paketti-helm-package.el
@@ -0,0 +1,10 @@
+;;; paketti-helm-package.el -*- lexical-binding: t -*-
+
+(req 'helm-package)
+
+(provide 'paketti-helm-package)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
