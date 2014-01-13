a515fc39ac89cf43ed47edf49602f1ebc65e109b
mytoh <mytoh@gmail.com>
2014-01-13 11:04:40 +0900

    paketti-eshell-prompt.el: add colour reset

--- inits/paketit/paketti-eshell-prompt.el
+++ inits/paketit/paketti-eshell-prompt.el
@@ -13,6 +13,7 @@
 
 (cl-defun my-eshell-prompt-function ()
   (concat
+   "[0m"
    (my-eshell-propertize (abbreviate-file-name (eshell/pwd)) 'my-eshell-pwd-face)
    "\n"
    (my-eshell-propertize "X / _ / X" 'my-eshell-char-face)
