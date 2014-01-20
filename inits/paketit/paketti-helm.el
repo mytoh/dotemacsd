1c51d2ca4e6b5ca5263cb611c0e7d2876ee351ee
mytoh <mytoh@gmail.com>
2014-01-20 22:27:35 +0900

    paketit/paketti-helm.el: change keymaps

--- inits/paketit/paketti-helm.el
+++ inits/paketit/paketti-helm.el
@@ -6,36 +6,44 @@
      (enable-mode helm-adaptative-mode)
      (disable-mode helm-match-plugin-mode)
 
-     (global-set-key (kbd "C-c h") 'helm-mini)
-     (global-set-key (kbd "M-x") 'helm-M-x)
-     (global-set-key (kbd "C-c C-m") 'helm-M-x)
-
-     (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-     (global-set-key (kbd "C-M-z") 'helm-resume)
+     (define-key global-map (kbd "C-c h") 'helm-mini)
+     (define-key global-map (kbd "M-x") 'helm-M-x)
+     (define-key global-map (kbd "C-c C-m") 'helm-M-x)
+     (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+     (define-key global-map (kbd "C-M-z") 'helm-resume)
      (define-key global-map [remap find-file] 'helm-find-files)
      (define-key global-map [remap occur] 'helm-occur)
      (define-key global-map (kbd "C-x b") 'helm-buffers-list)
      (define-key global-map [remap list-buffers] 'helm-buffers-list)
-
+     (define-key global-map [remap jump-to-register] 'helm-register)
+     (define-key global-map [remap dabbrev-expand]   'helm-dabbrev)
+     (define-key global-map [remap find-tag]         'helm-etags-select)
 
      (define-key helm-map (kbd "C-M-n") 'helm-next-source)
      (define-key helm-map (kbd "C-M-p") 'helm-previous-source)
      (define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
      (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
-     (disable-option helm-candidate-number-limit)
      (set-option helm-idle-delay          0.01)
      (set-option helm-input-idle-delay    0.01)
      (set-option  helm-m-occur-idle-delay 0.01)
-     (enable-option helm-M-x-always-save-history)
-     (disable-option helm-quick-update)
      (set-option helm-default-external-file-browser "pcmanfm")
-     (disable-option helm-move-to-line-cycle-in-source)
+     (set-option  helm-buffers-favorite-modes (append helm-buffers-favorite-modes
+                                                      '(picture-mode artist-mode)))
      (enable-option enable-recursive-minibuffers)
+     (enable-option helm-M-x-always-save-history)
+     (enable-option enable-recursive-minibuffers)
+     (disable-option helm-quick-update)
+     (disable-option helm-candidate-number-limit)
+     (disable-option helm-move-to-line-cycle-in-source)
      ;;;; helm-files
      (enable-option helm-ff-lynx-style-map)
      (enable-option helm-ff-transformer-show-only-basename)
-     (disable-option  helm-ff-newfile-prompt-p)
+     (enable-option helm-ff-search-library-in-sexp)
+     (disable-option helm-ff-newfile-prompt-p)
+
+     ;;;; match plugin
+     (set-option helm-mp-highlight-delay 0.2)
      ;; disable auto completion
      ;; (setq helm-ff-auto-update-initial-value nil)
      (set-option helm-external-programs-associations
@@ -87,9 +95,9 @@
 
 ;; cmd-t
 ;; (req 'helm-cmd-t
-;;      (global-set-key (kbd "M-t") 'helm-cmd-t))
+;;      (define-key global-map (kbd "M-t") 'helm-cmd-t))
 ;; (req 'helm-C-x-b
-;;      (global-set-key [remap switch-to-buffer] 'helm-C-x-b))
+;;      (define-key global-map [remap switch-to-buffer] 'helm-C-x-b))
 
 ;; helm-ls-git
 (pak 'helm-ls-git
