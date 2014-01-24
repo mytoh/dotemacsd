05b1323f8c6b86c1f93983636598cdc0f577a449
mytoh <mytoh@gmail.com>
2014-01-24 23:45:05 +0900

    paketti-company-mode.el: modify faces for scroll bar

--- inits/paketit/paketti-company-mode.el
+++ inits/paketit/paketti-company-mode.el
@@ -4,7 +4,11 @@
      (define-key company-active-map (kbd "C-n") 'company-select-next)
      (define-key company-active-map (kbd "C-p") 'company-select-previous)
 
+     (set-option company-idle-delay 0.3)
+     (set-option company-begin-commands '(self-insert-command))
+     (add-hook 'after-init-hook 'global-company-mode)
 
+     ;; faces
      (set-face-attribute 'company-tooltip-selection nil
                          :background (mytoh:colour-hsl-to-hex 19 60 50))
 
@@ -15,17 +19,19 @@
      (set-face-attribute 'company-tooltip-common nil
                          :foreground (mytoh:colour-hsl-to-hex 170 70 70))
 
-
      (set-face-attribute 'company-tooltip-common-selection nil
                          :foreground "LightSkyBlue")
+
+     (set-face-attribute 'company-scrollbar-bg nil
+                         :background (face-foreground 'company-tooltip))
+
+     (set-face-attribute 'company-scrollbar-fg nil
+                         :background (face-background 'company-tooltip))
 
      (add-hook 'company-mode-hook
                (lambda ()
                  (setq company-lighter " 会社")))
 
-     (set-option company-idle-delay 0.3)
-     (set-option company-begin-commands '(self-insert-command))
-     (add-hook 'after-init-hook 'global-company-mode)
      )
 
 
