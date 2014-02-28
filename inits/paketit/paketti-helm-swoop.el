3363402e5275a71f743eb12a6e29d2634a657cdb
mytoh <mytoh@gmail.com>
2014-03-01 05:48:45 +0900

    * inits/paketit/paketti-helm-swoop.el: add keybindings

--- inits/paketit/paketti-helm-swoop.el
+++ inits/paketit/paketti-helm-swoop.el
@@ -1,9 +1,31 @@
 
 (pak 'helm-swoop
 
-     ;; ;; Change the keybinds to whatever you like :)
+     ;; キーバインドはお好みで
      (global-set-key (kbd "M-i") 'helm-swoop)
-     (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point))
+     (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+     (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+     (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+
+     ;; isearch実行中にhelm-swoopに移行
+     (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+
+     (with-eval-after-load 'helm-swoop
+       ;; helm-swoop実行中にhelm-multi-swoop-allに移行
+       (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop))
+
+     ;; Save buffer when helm-multi-swoop-edit complete
+     (setq helm-multi-swoop-edit-save t)
+
+     ;; 値がtの場合はウィンドウ内に分割、nilなら別のウィンドウを使用
+     (setq helm-swoop-split-with-multiple-windows nil)
+
+     ;; ウィンドウ分割方向 'split-window-vertically or 'split-window-horizontally
+     (setq helm-swoop-split-direction 'split-window-vertically)
+
+     ;; nilなら一覧のテキストカラーを失う代わりに、起動スピードをほんの少し上げる
+     (setq helm-swoop-speed-or-color t)
+     )
 
 
 (provide 'paketti-helm-swoop)
