1f1a961c01609ef8c454bd0dbf8fb03fa0598b77
mytoh <mytoh@gmail.com>
2014-03-02 05:38:00 +0900

    * inits/paketit/paketti-tabbar.el: add tabbar-modified face

--- inits/paketit/paketti-tabbar.el
+++ inits/paketit/paketti-tabbar.el
@@ -2,11 +2,11 @@
 (req 'tabbar
      ;; http://d.hatena.ne.jp/alfad/20100425/1272208744
      ;; http://idita.blog11.fc2.com/blog-entry-810.html
-     (setq tabbar-cycle-scope 'tabs)
-     (setq tabbar-use-images nil)
-     (setq tabbar-separator '(1.0)) ;; space between tabs
+     (set-option tabbar-cycle-scope 'tabs)
+     (set-option tabbar-use-images nil)
+     (set-option tabbar-separator '(0.2)) ;; space between tabs
      (tabbar-mode t)
-     ;; disbale buttons no left side
+     ;; disbale buttons on left side
      (dolist (btn '(tabbar-home-button
                     tabbar-buffer-home-button
                     tabbar-scroll-right-button
@@ -20,10 +20,14 @@
 
      (set-face-attribute 'tabbar-selected nil
                          :foreground "#ffffff"
-                         :background "#336bcb"
+                         :background "#7b7b8b"
                          :box nil)
      (set-face-attribute 'tabbar-unselected nil
                          :foreground "#7b7b8b"
+                         :background "#424242"
+                         :box '(:line-width 1 :color "gray14" :style nil))
+     (set-face-attribute 'tabbar-modified nil
+                         :foreground "#88bb77"
                          :background "#424242"
                          :box '(:line-width 1 :color "gray14" :style nil))
      (set-face-attribute 'tabbar-button nil
