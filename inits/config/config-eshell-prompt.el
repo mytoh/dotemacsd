851519fbe6a2e452c3b1ee783c86d3c49cd394e2
mytoh <mytoh@gmail.com>
2014-03-02 08:38:31 +0900

    * inits/config/config-eshell-prompt.el: add git branch name to prompt

--- inits/config/config-eshell-prompt.el
+++ inits/config/config-eshell-prompt.el
@@ -7,14 +7,26 @@
   '((t (:foreground "#e6ae2e")))
   "eshell prompt prompt character face")
 
+(defface muki:eshell-branch-face
+  '((t (:foreground "#36ae9e")))
+  "eshell prompt prompt branch face")
+
 ;; prompt
 (cl-defun muki:eshell-propertize (s f)
   (propertize s 'face f))
+
+(cl-defun muki:eshell-git-branch ()
+  (cl-letf ((branches (vc-git-branches)))
+    (if (null (car branches))
+        ""
+      (car (vc-git-branches)))))
 
 (cl-defun muki:eshell-prompt-function ()
   (concat
    "[0m"
    (muki:eshell-propertize (abbreviate-file-name (eshell/pwd)) 'muki:eshell-pwd-face)
+   "  "
+   (muki:eshell-propertize (muki:eshell-git-branch) 'muki:eshell-branch-face)
    "\n"
    (muki:eshell-propertize "X / _ / X" 'muki:eshell-char-face)
    " "))
