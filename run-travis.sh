c9f3d1a16b73767038a4eb8375daa57522e28709
mytoh <mytoh@gmail.com>
2014-03-22 11:48:16 +0900

    * run-travis.sh: add --debug-init option

--- run-travis.sh
+++ run-travis.sh
@@ -6,4 +6,4 @@ git clone git://github.com/mytoh/dotemacsd /tmp/dotemacsd
 ln -sfv /tmp/dotemacsd /home/travis/.emacs.d
 
 ${EMACS} --version
-${EMACS} --batch -l /home/travis/.emacs.d/init.el
+${EMACS} --batch --debug-init --load /home/travis/.emacs.d/init.el
