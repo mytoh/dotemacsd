f1aa62a9b0e39810570d37347c9534d6851ae4b4
mytoh <mytoh@gmail.com>
2014-03-22 10:55:40 +0900

    update

--- run-travis.sh
+++ run-travis.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-EMACS_CLEAN=-Q
-EMACS_BATCH=$(EMACS_CLEAN) --batch
-
 WORK_DIR=$(shell pwd)
 PACKAGE_NAME=$(shell basename $(WORK_DIR))
 TRAVIS_FILE=.travis.yml
 
-${EMACS} ${EMACS_BATCH} -l init.el
+${EMACS} --version
+${EMACS} -batch -Q -l init.el
