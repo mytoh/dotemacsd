0d181961f5dad00b4dc8fa8632558485eb644643
mytoh <mytoh@gmail.com>
2014-03-22 11:34:11 +0900

    * run-travis.sh: update

--- run-travis.sh
+++ run-travis.sh
@@ -2,5 +2,8 @@
 
 EMACS=${EMACS:-$(which emacs)}
 
+git clone git://github.com/mytoh/dotemacsd /tmp/dotemacsd
+ln -sfv /tmp/dotemacsd /home/travis/.emacs.d
+
 ${EMACS} --version
-${EMACS} --batch -Q -l init.el
+${EMACS} --batch
