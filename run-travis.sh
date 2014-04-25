41a0ccc0c8b93e9e9540ba5b5a156ca88ec7dc43
mytoh <mytoh@gmail.com>
2014-04-25 20:03:03 +0900

    * run-travis.sh: install vendle

--- run-travis.sh
+++ run-travis.sh
@@ -2,8 +2,17 @@
 
 EMACS=${EMACS:-$(which emacs)}
 
-git clone git://github.com/mytoh/dotemacsd /tmp/dotemacsd
-ln -sfv /tmp/dotemacsd /home/travis/.emacs.d
+install_dotemacsd() {
+    git clone git://github.com/mytoh/dotemacsd /tmp/dotemacsd
+    ln -sfv /tmp/dotemacsd /home/travis/.emacs.d
+}
+
+install_vendle() {
+    git clone git://github.com/mytoh/emacs/vendle /home/travis/projektit/emacs-vendle
+}
+
+install_dotemacsd
+install_vendle
 
 ${EMACS} --version
 ${EMACS} --batch --debug-init --load /home/travis/.emacs.d/init.el
