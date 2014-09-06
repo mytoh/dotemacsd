3a5956a974a805b0169c45632b6c6560832b2e03
mytoh <mytoh@gmail.com>
2014-09-06 23:56:14 +0900

    * inits/vendle/init-e2wm.el: toggle tabbar

--- inits/vendle/init-e2wm.el
+++ inits/vendle/init-e2wm.el
@@ -3,35 +3,41 @@
 ;;; Code:
 
 (req 'e2wm
-     (global-set-key (kbd "M-+") 'e2wm:start-management)
-     (req 'e2wm-vcs)
+  (global-set-key (kbd "M-+") 'e2wm:start-management)
+  (req 'e2wm-vcs)
 
-     (setq e2wm:def-plugin-clock-url "http://www.avtokei.jp/images/clocks/%H/%H%M.jpg\?1277461066")
-     (setq e2wm:def-plugin-clock-referer "http://www.avtokei.jp/index.html")
+  (setq e2wm:def-plugin-clock-url "http://www.avtokei.jp/images/clocks/%H/%H%M.jpg\?1277461066")
+  (setq e2wm:def-plugin-clock-referer "http://www.avtokei.jp/index.html")
 
-     ;; C-c ; m に magit パースペクティブ変更を割り当て
-     (e2wm:add-keymap
-      e2wm:pst-minor-mode-keymap
-      '(("prefix m" . e2wm:dp-magit))
-      e2wm:prefix-key)
+  ;; C-c ; m に magit パースペクティブ変更を割り当て
+  (e2wm:add-keymap
+   e2wm:pst-minor-mode-keymap
+   '(("prefix m" . e2wm:dp-magit))
+   e2wm:prefix-key)
 
-     ;; (req 'e2wm-direx
+  ;; (req 'e2wm-direx
+  ;;      (setq e2wm:c-code-winfo
+  ;;            '((:name main)
+  ;;              (:name files    :plugin direx)
+  ;;              (:name history :plugin history-list)
+  ;;              (:name imenu   :plugin imenu :default-hide nil)
+  ;;              (:name sub     :buffer "*info*" :default-hide t))))
 
-     ;;      (setq e2wm:c-code-winfo
-     ;;            '((:name main)
-     ;;              (:name files    :plugin direx)
-     ;;              (:name history :plugin history-list)
-     ;;              (:name imenu   :plugin imenu :default-hide nil)
-     ;;              (:name sub     :buffer "*info*" :default-hide t))))
+  ;;; dashboard
+  (setq e2wm:c-dashboard-plugins
+        '(clock top
+          (open :plugin-args (:command eshell :buffer "*eshell*"))
+          (open :plugin-args (:command doctor :buffer "*doctor*"))))
 
-     ;;; dashboard
+  ;;; [[https://github.com/supermomonga/dot-emacs/blob/master/package-config/e2wm.el]]
+  (cl-defun muki:tabbar-toggle ()
+    (if (tabbar-mode-on-p)
+        (tabbar-mode 0)
+      (tabbar-mode t)))
+  (add-hook 'e2wm:pre-start-hook 'muki:tabbar-toggle)
+  (add-hook 'e2wm:post-stop-hook 'muki:tabbar-toggle)
 
-     (setq e2wm:c-dashboard-plugins
-           '(clock top
-             (open :plugin-args (:command eshell :buffer "*eshell*"))
-             (open :plugin-args (:command doctor :buffer "*doctor*"))))
-
-     )
+  )
 
 
 (provide 'init-e2wm)
