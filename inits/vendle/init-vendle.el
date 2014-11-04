3c7b9b2c6fed632aef9baea6ceefd579a4d4fa2e
mytoh <mytoh@gmail.com>
2014-11-05 02:03:31 +0900

    * inits/vendle/init-vendle.el: add eww-lnum setting

--- inits/vendle/init-vendle.el
+++ inits/vendle/init-vendle.el
@@ -446,7 +446,10 @@
     (auto (eww-lnum-follow eww-lnum-universal) "eww-lnum")
     (after "eww"
         (define-key eww-mode-map "f" 'eww-lnum-follow)
-      (define-key eww-mode-map "F" 'eww-lnum-universal)))
+      (define-key eww-mode-map "F" 'eww-lnum-universal))
+    (after "eww-lnum"
+        (set-face-attribute 'eww-lnum-number nil
+         :foreground "#3355bb")))
 
 
   ;; (req 'vi-tilde-fringe
