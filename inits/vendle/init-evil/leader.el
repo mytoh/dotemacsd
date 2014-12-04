2e1532d5309f67d56f3c854d01e3f9d7e8c6a58c
mytoh <mytoh@gmail.com>
2014-12-04 17:51:56 +0900

    * inits/vendle/init-evil/leader.el: :heavy_plus_sign: add keybinding macro

--- inits/vendle/init-evil/leader.el
+++ inits/vendle/init-evil/leader.el
@@ -2,6 +2,34 @@
 
 ;;; Code:
 
+
+;; from spacemacs
+(defvar muki:evil-leader-key-binding-prefixes nil)
+(setq muki:evil-leader-key-binding-prefixes
+      '(("a" .  application)
+        ("av" . application-vendle)
+        ("b" .  buffer)
+        ("g" .  git)
+        ("hi" . help-info)
+        ("hd" . help-describe)
+        ("T" .  theme)
+        ("z" .  eshell-session)))
+
+(cl-defun muki:evil-leader-prefix-set-key (category key def)
+  (cl-letf* ((found (cl-find-if (lambda (x) (cl-equalp category (cdr x)))
+                                muki:evil-leader-key-binding-prefixes))
+             (prefix (if found (car found) nil)))
+    (when found
+      (evil-leader/set-key (concat prefix key) def))))
+
+(cl-defmacro muki:evil-leader-prefix-set-keys (category &rest body)
+  (declare (debug t)
+           (indent 1))
+  (and body
+       `(cl-locally
+            (muki:evil-leader-prefix-set-key ,category ,(car body) ,(cadr body))
+          (muki:evil-leader-prefix-set-keys ,category ,@(cddr body)))))
+
 (cl-defun muki:init-evil-leader ()
   (req 'evil-leader
     (global-evil-leader-mode)
@@ -16,45 +44,54 @@
       "q" 'evil-quit
       )
 
+    (muki:evil-leader-prefix-set-keys 'help-describe
+      "f" 'describe-function
+      "k" 'describe-key
+      "m" 'describe-mode
+      "v" 'describe-variable)
+
     (cl-defun switch-to-scratch-buffer ()
       (interactive)
       (switch-to-buffer
        (get-buffer-create "*scratch*")))
 
-    (evil-leader/set-key
-        "b K"  'muki:kill-other-buffers
-      "b n"  'switch-to-next-buffer
-      "b p"  'switch-to-prev-buffer
-      "b s" 'switch-to-scratch-buffer)
+    (muki:evil-leader-prefix-set-keys 'buffer
+      "K"  'muki:kill-other-buffers
+      "n"  'switch-to-next-buffer
+      "p"  'switch-to-prev-buffer
+      "s" 'switch-to-scratch-buffer)
 
     (liby 'eshell-session
-      (evil-leader/set-key
-          "z z" 'eshell-session:switch
-        "z c" 'eshell-session:buffer-new
-        "z n" 'eshell-session:buffer-next
-        "z p" 'eshell-session:buffer-prev))
+      (muki:evil-leader-prefix-set-keys 'eshell-session
+        "z" 'eshell-session:switch
+        "c" 'eshell-session:buffer-new
+        "n" 'eshell-session:buffer-next
+        "p" 'eshell-session:buffer-prev))
 
     (liby 'helm-ypv
-      (evil-leader/set-key "e y" 'helm-ypv))
+      (muki:evil-leader-prefix-set-keys 'application
+        "y" 'helm-ypv))
 
     (liby 'helm-alku
-      (evil-leader/set-key
-          "e h" 'helm-alku
-        "e c" 'helm-alku-program
-        "e n" 'helm-alku-vihko))
+      (muki:evil-leader-prefix-set-keys 'application
+        "h" 'helm-alku
+        "c" 'helm-alku-program
+        "n" 'helm-alku-vihko))
 
     (liby 'vendle
-      (evil-leader/set-key "e v u" 'vendle-update
-        "e v k" 'vendle-check
-        "e v c" 'vendle-clean
-        "e v l" 'helm-vendle))
+      (muki:evil-leader-prefix-set-keys 'application-vendle
+        "u" 'vendle-update
+        "k" 'vendle-check
+        "c" 'vendle-clean
+        "l" 'helm-vendle))
 
     (liby 'magit
-      (evil-leader/set-key
-          "e g" 'magit-status))
+      (muki:evil-leader-prefix-set-keys 'git
+        "s" 'magit-status))
 
     (liby 'helm-project-buffer
-      (evil-leader/set-key "b b" 'helm-project-buffer))
+      (muki:evil-leader-prefix-set-keys 'buffer
+        "b" 'helm-project-buffer))
 
     (liby 'helm
       (evil-leader/set-key
@@ -64,8 +101,9 @@
         "h t"  'helm-themes))
 
     (liby 'paradox
-      (evil-leader/set-key
-          "e p" 'paradox-list-packages))
+      (after 'init-paradox
+          (muki:evil-leader-prefix-set-keys 'application
+            "P" 'muki:paradox-list-packages)))
 
     (liby 'eww
       (evil-leader/set-key
@@ -73,8 +111,8 @@
         "e W" 'eww))
 
     (liby 'helm-git-grep
-      (evil-leader/set-key
-          "e r" 'helm-git-grep))
+      (muki:evil-leader-prefix-set-keys 'git
+        "r" 'helm-git-grep))
 
 
     ;; (defun-add-hook muki:evil-ace-jump-mode-setup (after-init-hook)
