8336368c12d0dd48b284f044f7e65fc84a7f2b75
mytoh <mytoh@gmail.com>
2014-12-11 20:26:05 +0900

    * inits/vendle/init-migemo.el: use liby

--- inits/vendle/init-migemo.el
+++ inits/vendle/init-migemo.el
@@ -2,18 +2,18 @@
 
 ;;; Code:
 
-(req 'migemo
-     (setq migemo-command "cmigemo")
-     (setq migemo-options '("-q" "--emacs"))
+(liby 'migemo
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs"))
 
-     ;; Set your installed path
-     (setq migemo-dictionary "/usr/local/share/cmigemo/utf-8/migemo-dict")
+  ;; Set your installed path
+  (setq migemo-dictionary "/usr/local/share/cmigemo/utf-8/migemo-dict")
 
-     (setq migemo-user-dictionary nil)
-     (setq migemo-regex-dictionary nil)
-     (setq migemo-coding-system 'utf-8-unix)
-     (load-library "migemo")
-     (migemo-init))
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  (migemo-init))
 
 (provide 'init-migemo)
 
