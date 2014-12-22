069b6c763afbf409a8df69d40decadef8bf56bc5
mytoh <mytoh@gmail.com>
2014-12-23 01:40:23 +0900

    add layer functionality

---
+++ inits/layer/system/freebsd/init.el
@@ -0,0 +1,23 @@
+ ;;; -*- coding: utf-8 -*-
+
+(when (string-equal system-type "berkeley-unix")
+  (cond ((string-equal "fish" (file-name-base (getenv "SHELL")))
+         (setenv "SHELL" "/bin/tcsh")
+         (setenv "PATH" (shell-command-to-string "tcsh -c 'echo $PATH'"))
+         (cl-letf ((my-paths (seq-map #'expand-file-name '(  "~/huone/bin" "~/.config/lehti/bin"
+                                                           "~/.config/loitsu/bin"))))
+           (seq-doseq (dir my-paths)
+             ;; sakito.jp/emacs/emacsshell.html
+             (when (and (file-directory-p dir) (not (member dir exec-path)))
+               (setenv "PATH" (concat dir ":" (getenv "PATH")))
+               (add-to-list 'exec-path dir)))))
+        (t
+         (cl-letf ((my-paths (seq-map #'expand-file-name '(  "~/huone/bin" "~/.config/lehti/bin"
+                                                           "~/.config/loitsu/bin"))))
+           (seq-doseq (dir my-paths)
+             ;; sakito.jp/emacs/emacsshell.html
+             (when (and (file-directory-p dir) (not (member dir exec-path)))
+               (setenv "PATH" (concat dir ":" (getenv "PATH")))
+               (add-to-list 'exec-path dir)))))))
+
+(message "fuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuucccccccccccccccckkkkkkkkkkkkk")
---
+++ inits/lib/muki-layer.el
@@ -0,0 +1,30 @@
+;;; muki-layer -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Code:
+
+(defcustom muki:layer-root
+  (expand-file-name "inits/layer"
+                    user-emacs-directory)
+  "layer root directory")
+
+(cl-defun muki:layer-load (category name)
+  (cl-letf* ((cat (symbol-name category))
+             (name (symbol-name name))
+             (path (expand-file-name
+                    (concat cat "/" name)
+                    muki:layer-root)))
+    (load (expand-file-name
+           "init.el"
+           path))))
+
+(cl-defun muki:layer-load-layers (category names)
+  (declare (indent 1))
+  (seq-each
+   (lambda (n)
+     (muki:layer-load category n))
+   names)
+  t)
+
+(provide 'muki-layer)
+
+;;; muki-layer.el ends here
