dc6d19e24deb8a51636f46317847cb43c5bb846f
mytoh <mytoh@gmail.com>
2014-12-25 15:42:30 +0900

    * inits/layer/vendle/package/init.el: remove some packages

--- inits/layer/vendle/package/init.el
+++ inits/layer/vendle/package/init.el
@@ -29,20 +29,20 @@
   ;;   (initchart-record-execution-time-of require feature))
 
 
-    ;;;;; migemo
+;;;;; migemo
   (req 'init-migemo)
 
-    ;;;;; diminish
+;;;;; diminish
   (req 'init-diminish)
 
     ;;;; org
-    ;;;;; outshine
+;;;;; outshine
   (req 'init-outshine)
 
-    ;;;;; navi
+;;;;; navi
   ;; (req 'navi-mode)
 
-  ;;;;; org-autolist
+;;;;; org-autolist
   (liby 'org-autolist
     (auto (org-autolist-mode) "org-autolist")
     (add-hook 'org-mode-hook (lambda () (org-autolist-mode))))
@@ -51,105 +51,70 @@
   ;; (req 'org-pretty-table
   ;;   (add-hook 'org-mode-hook
   ;;             'turn-on-org-pretty-table-mode))
-    ;;;;;
+;;;;;
 
-    ;;;;; scheme-keywords
+;;;;; scheme-keywords
   (liby 'scheme-keywords
     (auto (scheme-keywords-mode) "scheme-keywords")
     (add-hook 'scheme-mode-hook 'scheme-keywords-mode))
 
-    ;;;;; seijiseikana
+;;;;; seijiseikana
   (req 'seijiseikana)
 
-  (req 'init-flatline)
 
-    ;;;;; fish-mode
+;;;;; fish-mode
   (liby 'fish-mode
     (auto (fish-mode) "fish-mode")
     (add-to-list 'auto-mode-alist '("\\.fish\\'" . fish-mode))
     (add-to-list 'interpreter-mode-alist '("fish" . fish-mode)))
 
-    ;;;;; debug-print
+;;;;; debug-print
   (req 'debug-print
     (debug-print-init)
     (add-global-key "C-x C-e" 'debug-print-eval-last-sexp))
 
-  ;;;;; bookmark-extensions
+;;;;; bookmark-extensions
   (req 'bookmark-extensions)
 
-    ;;;;; lehtifile-mode
+;;;;; lehtifile-mode
   (liby 'lehtifile-mode
     (autoload 'lehtifile-mode "lehtifile-mode")
     (mode "/Lehtifile\\'"  'lehtifile-mode))
 
-    ;;;;; skk
-  ;; ディレクトリ指定
-  ;; ~/.emacs.d/ddskk/init is setting file
-  (set-option skk-user-directory (muki:user-emacs-directory "ddskk/"))
-  (req 'skk-autoloads
-    ;; C-x C-j で skk モードを起動
-    (add-global-key "C-x C-\\" 'skk-mode
-                    "C-x C-j" 'skk-mode
-                    ;; enable skk mode by pressing カタカナ/ひらがな key
-                    [hiragana-katakana] 'skk-mode)
-    ;; set default input method to skk
-    ;; (setq default-input-method "japanese-skk")
-    ;; .skk を自動的にバイトコンパイル
-    (enable-option skk-byte-compile-init-file)
-
-    ;; (req 'context-skk)
-    )
-
-    ;;;;; navi2ch
-  (req 'init-navi2ch)
-
-  (req 'navi2ch-file-mode
-    (setq navi2ch-file-open-with-shift-jis t))
-
-      ;;;;; rebuildfm
+;;;;; rebuildfm
   (liby 'rebuildfm
     (auto (rebuildfm) "rebuildfm"))
 
-  ;;;;; sos
+;;;;; sos
   (liby 'sos
     (auto (sos) "sos"))
 
-    ;;;;; eshell
+;;;;; eshell
   ;; (req 'eshell-session)
   (req 'mesh
     (turn-on-mesh-mode))
 
-    ;;;;; yon-chan
+;;;;; yon-chan
   (liby 'yon-chan
     (auto (yon-chan) "yon-chan"))
 
-    ;;;;; emms
-  ;; (req 'init-emms)
-  (req 'init-emms-mpd)
-
-    ;;;;; magit
-  (req 'init-magit)
-  (req 'init-git-modes)
-  (liby 'magit-overview
-    (auto (magit-overview) "magit-overview"))
-
-    ;;;;; company
+;;;;; company
   (req 'init-company-mode)
 
-  ;;;;; anzu
+;;;;; anzu
   (req 'init-anzu)
 
-  ;;;;; smartparens
+;;;;; smartparens
   (req 'init-smartparens)
 
-    ;;;;; haskell
+;;;;; haskell
   (req 'init-haskell-mode)
 
-    ;;;;; itail
+;;;;; itail
   (liby 'itail
     (auto (itail) "itail"))
 
-    ;;;;; quickrun
+;;;;; quickrun
   (req 'quickrun
     (cl-defun quickrun-sc (start end)
       (interactive "r")
@@ -165,7 +130,7 @@
                           :default "scheme"
                           :mode 'scheme-mode))
 
-  ;;;;; web-mode
+;;;;; web-mode
   (liby 'web-mode
     (auto (web-mode) "web-mode")
     (mode "\\.phtml\\'"  'web-mode)
@@ -177,43 +142,43 @@
     (mode "\\.djhtml\\'"  'web-mode)
     (mode "\\.html?\\'"  'web-mode))
 
-    ;;;;; paradox
+;;;;; paradox
   (req 'init-paradox)
 
-    ;;;;; spray
+;;;;; spray
   ;; (req 'spray)
 
-    ;;;;; highlight-parentheses
+;;;;; highlight-parentheses
   (req 'highlight-parentheses
     (global-highlight-parentheses-mode))
 
-    ;;;;; go-mode
+;;;;; go-mode
   (req 'go-mode)
 
-    ;;;;; expand-region
+;;;;; expand-region
   (req 'init-expand-region)
 
-    ;;;;; lua-mode
+;;;;; lua-mode
   (liby 'lua-mode
     (auto (lua-mode) "lua-mode" )
     (mode "\\.lua$" 'lua-mode)
     (add-to-list 'interpreter-mode-alist '("lua" . lua-mode)))
 
-    ;;;;; rotate
+;;;;; rotate
   (liby 'rotate
     (auto (rotate-layout rotate-window) "rotate")
     (add-global-key "C-c C-SPC" 'rotate-layout
                     "C-c C-c C-SPC" 'rotate-window))
 
-    ;;;;; git-timemachine
+;;;;; git-timemachine
   (liby 'git-timemachine
     (auto (git-timemachine) "git-timemachine"))
 
-  ;;;;; flappymacs
+;;;;; flappymacs
   (liby 'flappymacs
     (auto (flappymacs) "flappymacs"))
 
-    ;;;;; yamada
+;;;;; yamada
   (liby 'yamada
     (auto (yamada) "yamada")
 
@@ -223,11 +188,11 @@
     ;; (yamada-dancing 5) ;; or C-u 5 C-c C-c C-y
     )
 
-  ;;;;; css-eldoc
+;;;;; css-eldoc
   (req 'css-eldoc
     (turn-on-css-eldoc))
 
-    ;;;;; web-beatify
+;;;;; web-beatify
   (liby 'web-beautify
     (auto (web-beautify-css-buffer) "web-beautify")
     (after 'css-mode
@@ -235,26 +200,26 @@
          (lambda ()
            (add-hook 'before-save-hook 'web-beautify-css-buffer t t)))))
 
-    ;;;;; direx
+;;;;; direx
   (liby 'direx
     (auto (direx:jump-to-directory
            direx:jump-to-directory-other-window)
           "direx"))
 
-    ;;;;; e2wm
+;;;;; e2wm
   (req 'init-e2wm)
 
-    ;;;;; oniisama
+;;;;; oniisama
   (req 'oniisama
     (imouto))
 
-    ;;;;; sunrise-commander
+;;;;; sunrise-commander
   (req 'init-sunrise-commander)
 
-    ;;;;; tabbar
+;;;;; tabbar
   ;; (req 'init-tabbar)
 
-    ;;;;; guide-key
+;;;;; guide-key
   (req 'guide-key
     (setq guide-key/guide-key-sequence
           `("C-x r" "C-x 4" "C-q"
@@ -265,30 +230,30 @@
     ;; (guide-key-mode 1)
     )
 
-    ;;;;; guide-key-tip
+;;;;; guide-key-tip
   (req 'guide-key-tip
     (setq guide-key-tip/enabled t))
 
-    ;;;;; clojure-mode
+;;;;; clojure-mode
   (req 'clojure-mode
     (req 'clojure-mode-extra-font-locking))
 
-    ;;;;; rainbow-delimiters
+;;;;; rainbow-delimiters
   (req 'init-rainbow-delimiters)
 
-  ;;;;; bqlist-lock
+;;;;; bqlist-lock
   (liby 'bqlist-lock
     (auto (bqlist-lock-enable) "bqlist-lock")
     (hook 'emacs-lisp-mode-hook 'bqlist-lock-enable))
 
-  ;;;;; elfeed
+;;;;; elfeed
   (req 'init-elfeed)
 
-    ;;;;; lorem-ipsum
+;;;;; lorem-ipsum
   (liby 'lorem-ipsum
     (auto (lorem-ipsum) "lorem-ipsum"))
 
-  ;;;;; flycheck
+;;;;; flycheck
   (liby 'flycheck-package
     (auto (flycheck-package-setup) "flycheck-package")
     (after 'flycheck
@@ -299,38 +264,34 @@
   ;; ;;;;; emacs-color-themes
   (req 'emacs-color-themes)
 
-  ;;;;; multiple-cursors
+;;;;; multiple-cursors
   (req 'init-multiple-cursors)
 
 ;;;;; highlight-symbol
   (req 'init-highlight-symbol)
 
-  ;;;;; sly
+;;;;; sly
   (req 'sly-autoloads
     (setq inferior-lisp-program "sbcl"))
 
-  ;;;;; twittering-mode
+;;;;; twittering-mode
   (liby 'twittering-mode
     (auto (twit) "twittering-mode")
     (setq twittering-allow-insecure-server-cert t))
 
-  ;;;;; coffee
+;;;;; coffee
   (req 'coffee-mode)
 
-  ;;;;; artbollocks
-  (liby 'artbollocks-mode
-    (auto (artbollocks-mode) "artbollocks-mode")
-    (add-hook 'text-mode-hook 'artbollocks-mode))
 
-  ;;;;; color-theme-approximate
+;;;;; color-theme-approximate
   (req 'color-theme-approximate
     (color-theme-approximate-on))
 
-  ;;;;; google-this
+;;;;; google-this
   (liby 'google-this
     (auto (google-this) "google-this"))
 
-  ;;;;; golden-ratio
+;;;;; golden-ratio
   ;; (req 'golden-ratio
   ;;   (enable-mode golden-ratio-mode)
   ;;   (add-to-list 'golden-ratio-exclude-modes 'eshell-mode)
@@ -338,40 +299,34 @@
   ;;   (add-to-list 'golden-ratio-exclude-modes 'magit-log-mode)
   ;;   (add-to-list 'golden-ratio-exclude-modes 'magit-mode))
 
-  ;;;;; nssh
+;;;;; nssh
   (liby 'nssh
     (auto (nssh nssh-all) "nssh"))
 
-  ;;;;; popwin
+;;;;; popwin
   (req 'init-popwin)
   ;; (req 'init-shackle)
 
-  ;;;;; tumblesocks
-  (req 'init-tumblesocks)
-
-  ;;;;; rainbow-mode
-  (req 'init-rainbow-mode)
-
-  ;;;;; markdown
+;;;;; markdown
   (liby 'markdown-mode
     (auto (gfm-mode markdown-mode) "markdown-mode")
     (mode "\\.md\\'" 'gfm-mode)
     (mode "\\.markdown\\'" 'gfm-mode)
     (mode "README\\.md\\'"  'gfm-mode))
 
-  ;;;;; google-translate
+;;;;; google-translate
   (req 'init-google-translate)
 
-  ;;;;; charmap
+;;;;; charmap
   (liby 'charmap
     (auto (charmap) "charmap")
     (setq charmap-text-scale-adjust 2))
 
-  ;;;;; pt
+;;;;; pt
   (liby 'pt
     (auto (pt-regexp) "pt"))
 
-  ;;;;; switch-window
+;;;;; switch-window
   (liby 'switch-window
     (auto (switch-window) "switch-window")
     (global-set-key (kbd "C-x o") 'switch-window)
@@ -379,141 +334,15 @@
     (set-option switch-window-qwerty-shortcuts
                 '("a" "o" "e" "u" "h" "t" "n" "s" "," "." "c" "r")))
 
-  ;;;;; olivetti
-  (liby 'olivetti
-    (auto (olivetti-mode) "olivetti"))
-
-  ;;;;; emoji-cheat-sheet
-  (liby 'emoji-cheat-sheet
-    (auto (emoji-cheat-sheet emoji-cheat-sheet-copy-at-point)
-          "emoji-cheat-sheet"))
-
-  ;;;;; vimrc
-  (liby 'vimrc-mode
-    (auto (vimrc-mode) "vimrc-mode")
-    (mode "\\.vim\\'" 'vimrc-mode)
-    (mode "[._]?g?vimrc\\'"  'vimrc-mode)
-    (mode "\\.exrc\\'"  'vimrc-mode))
-
-  ;;;;; cat-mode
-  (req 'cat-mode)
-
-  ;;;;; rich-minority
-  (req 'rich-minority
-    (add-to-list 'rm-text-properties
-                 '("Golden" 'face 'font-lock-variable-face))
-    (rich-minority-mode 1))
-
-  ;;;;; highlight-defined
-  (liby 'highlight-defined
-    (auto (highlight-defined-mode) "highlight-defined")
-    (add-hook 'emacs-lisp-mode-hook 'highlight-defined-mode))
-
-;;;;; httprepl
-  (liby 'httprepl
-    (auto (httprepl) "httprepl"))
-
-  ;;;;; yaml-mode
-  (req 'yaml-mode)
-
-  ;;;;; info+
-  (req 'init-info-plus)
-
-  ;;;;; elisp-slime-nav
-  (req 'init-elisp-slime-nav)
-
-  ;;;;; ace-link
-  (req 'ace-link
-    (ace-link-setup-default))
-
-  ;;;;; esup
-  (liby 'esup
-    (auto (esup) "esup" ))
-
-  ;;;;; context-coloring
-  (liby 'context-coloring
-    (auto (context-coloring-mode) "context-coloring")
-    (add-hook 'js-mode-hook 'context-coloring-mode))
-
-  ;;;;; stumpwm
-  (liby 'stumpwm-mode
-    (auto (stumpwm-mode) "stumpwm-mode")
-    (mode ".stumpwmrc.d/conf" 'stumpwm-mode))
-
-  ;;;;; smeargle
-  (liby 'smeargle
-    (auto (smeargle-clear smeargle smeargle-commits) "smeargle"))
-
-  ;;;;; stack exchange mode
-  (req 'sx-load)
-
-  ;;;;; csharp-mode
+;;;;; csharp-mode
   ;; (req 'csharp-mode)
-
-  ;;;;; dircmp
-  (req 'dircmp)
-
-  ;;;;; url-preview
-  (req 'url-preview
-    (req 'url-preview-image
-      (url-preview-module-enable "image")))
-
-  ;;;;; stripe-buffer
-  (liby 'stripe-buffer
-    (auto (stripe-listify-buffer turn-on-stripe-table-mode) "stripe-buffer")
-    (add-hook 'dired-mode-hook 'stripe-listify-buffer)
-    (add-hook 'org-mode-hook 'turn-on-stripe-table-mode))
-
-  ;;;;; free-keys
-  (liby 'free-keys
-    (auto (free-keys) "free-keys"))
-
-  ;;;;; better-jump
-  (liby 'better-jump
-    (auto (bjump-char-jump-line
-           bjump-word-jump
-           bjump-word-jump-line
-           bjump-word-jump-paragraph
-           bjump-window-jump
-           bjump-window-delete
-           bjump-help-link-jump
-           bjump-info-link-jump
-           bjump-dired-jump)
-          "better-jump"))
-
-  ;;;;; tabula-rasa
-  (liby 'tabula-rasa
-    (auto (tabula-rasa-mode) "tabula-rasa")
-    (set-option tabula-rasa-widith 130))
-
-  ;;;;; darkroom
-  (liby 'darkroom
-    (auto (darkroom-mode darkroom-tentative-mode)
-          "darkroom"))
-
-  ;;;;; leerzeichen-mode
-  (liby 'leerzeichen
-    (auto (leerzeichen-mode) "leerzeichen")
-    (after "leerzeichen"
-        (set-face-attribute 'leerzeichen nil
-         :foreground
-         (color-lighten-name (face-attribute 'default :background)
-                             20))))
-
-  ;;;;; emacs-refactor
-  (liby 'emr
-    (autoload 'emr-show-refactor-menu "emr")
-    (autoload 'emr-initialize "emr")
-    (define-key prog-mode-map (kbd "M-RET") 'emr-show-refactor-menu)
-    (add-hook 'prog-mode-hook 'emr-initialize))
-
 
   ;;; util
   (cl-defun vendle-find-duplicate-packages ()
     (seq-filter
      (lambda (p)
        (cl-find-if (lambda (v) (equalp (vendle:package-name v)
-                                  p))
+                                       p))
                    *vendle-package-list*))
      (seq-map
       (lambda (p) (format "%s" p))
