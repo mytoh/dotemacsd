3e1c30e61e7468d312ca5f4ca7dacb590d3d5c86
mytoh <mytoh@gmail.com>
2014-12-30 06:28:30 +0900

    * inits/layer/evil/evil-leader/init.el: use sharp quote for functions

--- inits/layer/evil/evil-leader/init.el
+++ inits/layer/evil/evil-leader/init.el
@@ -39,22 +39,22 @@
     (enable-option evil-leader/in-all-states)
 
     (evil-leader/set-key
-        "v r" 'revert-buffer
-      "v e" 'pp-eval-last-sexp
-      "w" 'evil-write
-      "Q" 'evil-quit-all
-      "q" 'kill-buffer-and-window
+        "v r" #'revert-buffer
+      "v e" #'pp-eval-last-sexp
+      "w" #'evil-write
+      "Q" #'evil-quit-all
+      "q" #'kill-buffer-and-window
       )
 
     (muki:evil-leader-prefix-set-keys 'eval
-      "e" 'eval-last-sexp
-      "x" 'eval-defun)
+      "e" #'eval-last-sexp
+      "x" #'eval-defun)
 
     (muki:evil-leader-prefix-set-keys 'help-describe
-      "f" 'describe-function
-      "k" 'describe-key
-      "m" 'describe-mode
-      "v" 'describe-variable)
+      "f" #'describe-function
+      "k" #'describe-key
+      "m" #'describe-mode
+      "v" #'describe-variable)
 
     (cl-defun switch-to-scratch-buffer ()
       (interactive)
@@ -62,65 +62,65 @@
        (get-buffer-create "*scratch*")))
 
     (muki:evil-leader-prefix-set-keys 'buffer
-      "K"  'muki:kill-other-buffers
-      "n"  'switch-to-next-buffer
-      "p"  'switch-to-prev-buffer
-      "s" 'switch-to-scratch-buffer)
+      "K"  #'muki:kill-other-buffers
+      "n"  #'switch-to-next-buffer
+      "p"  #'switch-to-prev-buffer
+      "s" #'switch-to-scratch-buffer)
 
     ;; (liby 'eshell-switch
     ;;   (muki:evil-leader-prefix-set-keys 'eshell-switch
-    ;;     "z" 'eshell-switch:switch
-    ;;     "c" 'eshell-switch:buffer-new
-    ;;     "n" 'eshell-switch:buffer-next
-    ;;     "p" 'eshell-switch:buffer-prev))
+    ;;     "z" #'eshell-switch:switch
+    ;;     "c" #'eshell-switch:buffer-new
+    ;;     "n" #'eshell-switch:buffer-next
+    ;;     "p" #'eshell-switch:buffer-prev))
 
     (liby 'mesh
       (muki:evil-leader-prefix-set-keys 'mesh
-        "z" 'mesh:switch)
+        "z" #'mesh:switch)
       (evil-leader/set-key-for-mode 'eshell-mode
-          "zc" 'mesh:new-tab
-          "zn" 'mesh:next-tab
-          "zp" 'mesh:prev-tab
-          "zs" 'mesh:split-tab
-          "zS" 'mesh:vsplit-tab
-          "zx" 'mesh:kill-pane
-          "z)" 'mesh:next-session
-          "z(" 'mesh:prev-session
-          "zC" 'mesh:new-session
-          "z <tab>" 'mesh:next-pane))
+          "zc" #'mesh:new-tab
+          "zn" #'mesh:next-tab
+          "zp" #'mesh:prev-tab
+          "zs" #'mesh:split-tab
+          "zS" #'mesh:vsplit-tab
+          "zx" #'mesh:kill-pane
+          "z)" #'mesh:next-session
+          "z(" #'mesh:prev-session
+          "zC" #'mesh:new-session
+          "z <tab>" #'mesh:next-pane))
 
     (liby 'helm-ypv
       (muki:evil-leader-prefix-set-keys 'application
-        "y" 'helm-ypv))
+        "y" #'helm-ypv))
 
     (liby 'helm-alku
       (muki:evil-leader-prefix-set-keys 'application
-        "h" 'helm-alku
-        "c" 'helm-alku-program
-        "n" 'helm-alku-vihko))
+        "h" #'helm-alku
+        "c" #'helm-alku-program
+        "n" #'helm-alku-vihko))
 
     (liby 'vendle
       (muki:evil-leader-prefix-set-keys 'application-vendle
-        "u" 'vendle-update
-        "k" 'vendle-check
-        "c" 'vendle-clean
-        "l" 'helm-vendle))
+        "u" #'vendle-update
+        "k" #'vendle-check
+        "c" #'vendle-clean
+        "l" #'helm-vendle))
 
     (liby 'magit
       (muki:evil-leader-prefix-set-keys 'git
-        "s" 'magit-status))
+        "s" #'magit-status))
 
     (liby 'helm-project-buffer
       (muki:evil-leader-prefix-set-keys 'buffer
-        "b" 'helm-project-buffer))
+        "b" #'helm-project-buffer))
 
     (liby 'helm
       (evil-leader/set-key
-          ";" 'helm-M-x
-        "?" 'helm-descbinds
-        "h s"   'helm-swoop)
+          ";" #'helm-M-x
+        "?" #'helm-descbinds
+        "h s"   #'helm-swoop)
       (muki:evil-leader-prefix-set-keys 'theme
-        "h" 'helm-themes))
+        "h" #'helm-themes))
 
     (liby 'paradox
       (muki:evil-leader-prefix-set-keys 'application
@@ -128,24 +128,24 @@
 
     (liby 'eww
       (muki:evil-leader-prefix-set-keys 'application
-        "w" 'muki:eww
-        "W" 'eww))
+        "w" #'muki:eww
+        "W" #'eww))
 
     (liby 'helm-git-grep
       (muki:evil-leader-prefix-set-keys 'git
-        "r" 'helm-git-grep))
+        "r" #'helm-git-grep))
 
     (liby 'emms
       (muki:evil-leader-prefix-set-keys 'emms
-        "P" 'emms-player-mpd-pause
-        "s" 'emms-player-mpd-stop
-        "p" 'emms-player-mpd-previous
-        "n" 'emms-player-mpd-next
-        "b" 'emms-smart-browse
-        "a f" 'emms-add-file
-        "a d" 'emms-add-directory-tree
-        "+" 'emms-volume-raise
-        "-" 'emms-volume-lower))
+        "P" #'emms-player-mpd-pause
+        "s" #'emms-player-mpd-stop
+        "p" #'emms-player-mpd-previous
+        "n" #'emms-player-mpd-next
+        "b" #'emms-smart-browse
+        "a f" #'emms-add-file
+        "a d" #'emms-add-directory-tree
+        "+" #'emms-volume-raise
+        "-" #'emms-volume-lower))
 
     ;; (defun-add-hook muki:evil-ace-jump-mode-setup (after-init-hook)
     ;;   (when (and (featurep 'evil) (featurep 'evil-leader))
