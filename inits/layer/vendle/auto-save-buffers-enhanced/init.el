3158821cb736d7c1dca0acaf84aec44839da8d17
mytoh <mytoh@gmail.com>
2015-02-09 23:50:38 +0900

    * inits/layer/vendle/auto-save-buffers-enhanced/init.el: update setting

--- inits/layer/vendle/auto-save-buffers-enhanced/init.el
+++ inits/layer/vendle/auto-save-buffers-enhanced/init.el
@@ -3,6 +3,11 @@
 ;;; Code:
 
 (req 'auto-save-buffers-enhanced
+  (set-option auto-save-buffers-enhanced-interval
+              10)
+  ;; exclude some files
+  (set-option auto-save-buffers-enhanced-exclude-regexps
+              '("COMMIT_EDITMSG\\'"))
   ;; If you're using CVS or Subversion or git
   (auto-save-buffers-enhanced-include-only-checkout-path t)
   (auto-save-buffers-enhanced t))
