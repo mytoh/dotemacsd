5189840314001eca6a65c4a1898b40d350e2600f
mytoh <mytoh@gmail.com>
2015-02-18 17:16:52 +0900

    * inits/layer/vendle/mpv/init.el: fix

--- inits/layer/vendle/mpv/init.el
+++ inits/layer/vendle/mpv/init.el
@@ -3,12 +3,13 @@
 ;;; Code:
 
 (req 'mpv
-  (org-add-link-type "mpv" #'mpv-play)
-  (defun org-mpv-complete-link (&optional arg)
-    (replace-regexp-in-string
-     "file:" "mpv:"
-     (org-file-complete-link arg)
-     t t))
-  (add-hook 'org-open-at-point-functions #'mpv-seek-to-position-at-point))
+  (after 'org
+      (org-add-link-type "mpv" #'mpv-play)
+    (defun org-mpv-complete-link (&optional arg)
+      (replace-regexp-in-string
+       "file:" "mpv:"
+       (org-file-complete-link arg)
+       t t))
+    (add-hook 'org-open-at-point-functions #'mpv-seek-to-position-at-point)))
 
 ;;; init.el ends here
