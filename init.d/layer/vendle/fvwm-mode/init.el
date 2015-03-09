816325b713af692d5bf049fe357df7f650f4eb1a
mytoh <mytoh@gmail.com>
2015-03-09 14:15:51 +0900

    * init.d/layer/vendle/fvwm-mode/init.el: add fvwm-mode

---
+++ init.d/layer/vendle/fvwm-mode/init.el
@@ -0,0 +1,12 @@
+;;; init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Code:
+
+(liby 'fvwm-mode
+  (auto (fvwm-mode) "fvwm-mode")
+  (dolist (pattern '("/home/[^/]*/\\.fvwm/config\\'"
+                     "/home/[^/]*\\(/\\|\\/.fvwm/\\).fvwm2rc\\'" "\\.fvwm\\'"
+                     "/ConfigFvwm" "/FvwmScript-" "/FvwmForm-" "/FvwmTabs-"))
+    (add-to-list 'auto-mode-alist (cons pattern 'fvwm-mode))))
+
+;;; init.el ends here
