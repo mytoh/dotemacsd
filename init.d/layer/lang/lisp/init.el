ecd1076eb30bb11aa0d4704838717233d27c1062
mytoh <mytoh@gmail.com>
2015-03-14 11:30:52 +0900

    * init.d/layer/lang/lisp/init.el: add ignore-errors

--- init.d/layer/lang/lisp/init.el
+++ init.d/layer/lang/lisp/init.el
@@ -9,7 +9,7 @@
 (cl-defun muki:lisp-cleanup-support (regex replace)
   (save-excursion
     (save-match-data
-      (while (re-search-forward regex nil t)
+      (while (ignore-errors (re-search-forward regex nil t))
         (unless (muki:in-string-or-comment)
           (replace-match replace))))))
 
