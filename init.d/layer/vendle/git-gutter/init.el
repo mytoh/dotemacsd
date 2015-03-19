b148d0ea92926bd068deb1458fff06b511b3e415
mytoh <mytoh@gmail.com>
2015-03-19 19:01:35 +0900

    * init.d/layer/vendle/git-gutter/init.el: update

--- init.d/layer/vendle/git-gutter/init.el
+++ init.d/layer/vendle/git-gutter/init.el
@@ -5,7 +5,15 @@
   (setq git-gutter:added-sign "☀")
   (setq git-gutter:deleted-sign "☂")
   (setq git-gutter:disabled-modes '(image-mode eshell-mode))
+
   (add-hook 'emacs-lisp-mode-hook #'git-gutter-mode)
+
+  ;; diff information is updated at hooks in git-gutter:update-hooks.
+  (add-to-list 'git-gutter:update-hooks 'focus-in-hook)
+  (add-to-list 'git-gutter:update-hooks 'evil-insert-state-exit-hook)
+  ;; diff information is updated after command in git-gutter:update-commands executed.
+  (add-to-list 'git-gutter:update-commands #'other-window)
+
   (after 'diminish
       (after 'git-gutter
           (diminish 'git-gutter-mode "")))
