d6962f89bede4646fa090673d983ec9c0801a2ad
mytoh <mytoh@gmail.com>
2015-04-06 17:26:12 +0900

    update

--- init.d/layer/vendle/magit/init.el
+++ init.d/layer/vendle/magit/init.el
@@ -1,14 +1,15 @@
 
 (cl-defun muki:magit-highlights ()
-  (set-face-attribute 'magit-diff-add nil
-                      :foreground (face-foreground 'font-lock-doc-face)
-                      :background nil)
+  ;; (set-face-attribute 'magit-diff-add nil
+  ;;                     :foreground (face-foreground 'font-lock-doc-face)
+  ;;                     :background nil)
   ;; (set-face-attribute 'diff-refine-added nil :foreground "green3" :background nil :inherit nil)
   ;; (set-face-attribute 'magit-diff-del nil :foreground "red3" :background nil)
   ;; (set-face-attribute 'diff-refine-removed nil :foreground "red3"  :background nil :inherit nil)
   ;; (set-face-attribute 'magit-diff-file-header nil :foreground "#4040ff" :background nil)
   ;; (set-face-attribute 'magit-item-highlight nil :foreground nil)
-  (set-face-background 'magit-item-highlight "#121212"))
+  ;; (set-face-background 'magit-item-highlight "#121212")
+  )
 
 (after 'magit
     (muki:magit-highlights))
