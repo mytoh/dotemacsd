875dcd1c56aabab1e97e4e4c0c482fc7c42337f5
mytoh <mytoh@gmail.com>
2015-04-06 22:32:28 +0900

    update

--- init.d/layer/vendle/company/init.el
+++ init.d/layer/vendle/company/init.el
@@ -1,45 +1,50 @@
 
-(req 'company
+(liby 'company
+      (command (company-mode) "company")
 
-  ;; (setq company-idle-delay 0.3)
-  (setq company-tooltip-limit 30)
-  (setq company-minimum-prefix-length 2)
-  ;; (setq company-echo-delay 0)
-  ;; (setq company-auto-complete nil)
-  ;; (add-hook 'after-init-hook #'global-company-mode)
-  (add-hook 'emacs-lisp-mode-hook #'company-mode)
-  (add-hook 'scheme-mode-hook #'company-mode)
-  (add-hook 'org-mode-hook #'company-mode)
-  (after 'magit
-      (add-hook 'magit-commit-mode #'company-mode))
-  (cl-pushnew 'company-dabbrev company-backends)
-  (cl-pushnew 'company-ispell company-backends)
-  (cl-pushnew 'company-files  company-backends)
 
-  (add-key company-active-map
-    "C-n" #'company-select-next
-    "C-p" #'company-select-previous)
+      ;; (add-hook 'after-init-hook #'global-company-mode)
+      (add-hook 'emacs-lisp-mode-hook #'company-mode)
+      (add-hook 'scheme-mode-hook #'company-mode)
+      (add-hook 'org-mode-hook #'company-mode)
+      (after 'magit
+             (add-hook 'magit-commit-mode #'company-mode))
 
-  (set-face-attribute 'company-tooltip-selection nil
-                      :background (muki:colour-hsl-to-hex 19 60 50))
+      (after 'company
+             ;; (setq company-idle-delay 0.3)
+             (setq company-tooltip-limit 30)
+             (setq company-minimum-prefix-length 2)
 
-  (set-face-attribute 'company-tooltip nil
-                      :foreground "#dfdfe1"
-                      :background "#393939")
+             (add-key company-active-map
+                      "C-n" #'company-select-next
+                      "C-p" #'company-select-previous)
 
-  (set-face-attribute 'company-tooltip-common nil
-                      :foreground (muki:colour-hsl-to-hex 170 70 70))
+             (set-face-attribute 'company-tooltip-selection nil
+                                 :background (muki:colour-hsl-to-hex 19 60 50))
 
-  (set-face-attribute 'company-tooltip-common-selection nil
-                      :foreground "LightSkyBlue")
+             (set-face-attribute 'company-tooltip nil
+                                 :foreground "#dfdfe1"
+                                 :background "#393939")
 
-  (set-face-attribute 'company-scrollbar-bg nil
-                      :background (face-foreground 'company-tooltip))
+             (set-face-attribute 'company-tooltip-common nil
+                                 :foreground (muki:colour-hsl-to-hex 170 70 70))
 
-  (set-face-attribute 'company-scrollbar-fg nil
-                      :background (face-background 'company-tooltip))
+             (set-face-attribute 'company-tooltip-common-selection nil
+                                 :foreground "LightSkyBlue")
 
-  ;; (req 'company-scheme
-  ;;   (add-to-list 'company-backends 'company-scheme-backend t))
+             (set-face-attribute 'company-scrollbar-bg nil
+                                 :background (face-foreground 'company-tooltip))
 
-  )
+             (set-face-attribute 'company-scrollbar-fg nil
+                                 :background (face-background 'company-tooltip))
+
+             )
+      ;; (setq company-echo-delay 0)
+      ;; (setq company-auto-complete nil)
+      ;; (cl-pushnew 'company-dabbrev company-backends)
+      ;; (cl-pushnew 'company-ispell company-backends)
+      ;; (cl-pushnew 'company-files  company-backends)
+      ;; (req 'company-scheme
+      ;;   (add-to-list 'company-backends 'company-scheme-backend t))
+
+      )
