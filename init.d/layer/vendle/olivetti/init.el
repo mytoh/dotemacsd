f1ef3c767e3271a5203fd7be7040936c451c23d4
mytoh <mytoh@gmail.com>
2015-04-14 21:32:48 +0900

    copied setting

--- init.d/layer/vendle/olivetti/init.el
+++ init.d/layer/vendle/olivetti/init.el
@@ -4,7 +4,13 @@
 
 ;;;;; olivetti
 (liby 'olivetti
-  (command (olivetti-mode) "olivetti"))
+  (command (olivetti-mode) "olivetti")
+;;; 桁数
+  (setq olivetti-body-width 80)           ;桁数
+  ;; (setq olivetti-body-width 0.6)          ;(window-width) に対する割合
+;;; モードラインを隠す
+  (setq olivetti-hide-mode-line t)
+  )
 
 
 ;;; init.el ends here
