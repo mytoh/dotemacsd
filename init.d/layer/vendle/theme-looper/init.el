cf35c8955129ec6a9bf5f9164899c6497e4f11ed
mytoh <mytoh@gmail.com>
2015-04-20 06:08:51 +0900

    fix theme-looper setting

--- init.d/layer/vendle/theme-looper/init.el
+++ init.d/layer/vendle/theme-looper/init.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 (req 'theme-looper
-  (tl:set-theme-set
+  (theme-looper:set-theme-set
    '(comidia planet monokai molokai subatomic256 birds-of-paradise-plus firebelly zen-and-art gruvbox base16-tomorrow
-     spolsky odersky jazz purple-haze warm-night brin gotham soothe spolsky wilson noctilux erosiond material)))
+             spolsky odersky jazz purple-haze warm-night brin gotham soothe spolsky wilson noctilux erosiond material)))
 
 ;;; init.el ends here
