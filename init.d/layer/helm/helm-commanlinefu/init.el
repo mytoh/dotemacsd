d46349de031e35ad1b775fe9c0af57eb683d658e
mytoh <mytoh@gmail.com>
2015-05-31 14:34:55 +0900

    add package settings

---
+++ init.d/layer/helm/helm-commanlinefu/init.el
@@ -0,0 +1,9 @@
+;;; init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Code:
+
+(liby 'helm-commandlinefu
+  (command (helm-commandlinefu-search helm-commandlinefu-browse)
+           "helm-commandlinefu"))
+
+;;; init.el ends here
---
+++ init.d/layer/vendle/longlines-jp/init.el
@@ -0,0 +1,8 @@
+;;; init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Code:
+
+(liby 'longlines-jp
+  (command (longlines-jp-mode) "longlines-jp"))
+
+;;; init.el ends here
