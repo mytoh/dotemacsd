5ec5687482dc94d79673b640a60f47765d958ad4
mytoh <mytoh@gmail.com>
2015-06-13 03:37:44 +0900

    fix hydra setting

--- init.d/layer/vendle/imagex/init.el
+++ init.d/layer/vendle/imagex/init.el
@@ -5,8 +5,15 @@
 
 (after 'image
     (req 'image+
-      (when (require 'hydra nil t)
-        (hydra-create "C-x C-l" imagex-hydra-default-heads))
+      (defhydra imagex-sticky-binding (global-map "C-x C-l")
+        "Manipulating Image"
+        ("+" imagex-sticky-zoom-in "zoom in")
+        ("-" imagex-sticky-zoom-out "zoom out")
+        ("M" imagex-sticky-maximize "maximize")
+        ("O" imagex-sticky-restore-original "restore original")
+        ("S" imagex-sticky-save-image "save file")
+        ("r" imagex-sticky-rotate-right "rotate right")
+        ("l" imagex-sticky-rotate-left "rotate left"))
       (imagex-global-sticky-mode)
       (imagex-auto-adjust-mode)))
 
