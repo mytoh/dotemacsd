f9959c7214e61a4738393a68cb786a014ae934f7
mytoh <mytoh@gmail.com>
2015-06-13 03:39:54 +0900

    check hydra library existence

--- init.d/layer/vendle/imagex/init.el
+++ init.d/layer/vendle/imagex/init.el
@@ -5,15 +5,16 @@
 
 (after 'image
     (req 'image+
-      (defhydra imagex-sticky-binding (global-map "C-x C-l")
-        "Manipulating Image"
-        ("+" imagex-sticky-zoom-in "zoom in")
-        ("-" imagex-sticky-zoom-out "zoom out")
-        ("M" imagex-sticky-maximize "maximize")
-        ("O" imagex-sticky-restore-original "restore original")
-        ("S" imagex-sticky-save-image "save file")
-        ("r" imagex-sticky-rotate-right "rotate right")
-        ("l" imagex-sticky-rotate-left "rotate left"))
+      (liby 'hydra
+        (defhydra imagex-sticky-binding (global-map "C-x C-l")
+          "Manipulating Image"
+          ("+" imagex-sticky-zoom-in "zoom in")
+          ("-" imagex-sticky-zoom-out "zoom out")
+          ("M" imagex-sticky-maximize "maximize")
+          ("O" imagex-sticky-restore-original "restore original")
+          ("S" imagex-sticky-save-image "save file")
+          ("r" imagex-sticky-rotate-right "rotate right")
+          ("l" imagex-sticky-rotate-left "rotate left")))
       (imagex-global-sticky-mode)
       (imagex-auto-adjust-mode)))
 
