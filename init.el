73e7f242b2a3e6bcbe36cf5c61f2f2aa576ca6c9
mytoh <mytoh@gmail.com>
2015-06-14 16:03:26 +0900

    set user-emacs-directory

--- init.el
+++ init.el
@@ -2,13 +2,23 @@
 
 (require 'cl-lib)
 
-;;;; gc
-(setq gc-cons-threshold (* 1024 1024 1024))
+(cl-defun setup-user-emacs-directory ()
+  ;; [[http://d.hatena.ne.jp/peccu/20130218/trial_init]]
+  ;; make emacs loadable like this
+  ;; $ emacs -q -l /path/to/init.el
+  ;;
+  ;; emacs -l init.el のように起動されるとload-file-nameにinit.elのパスが入るので
+  (when load-file-name
+    ;; 設定ファイルの基準となるディレクトリを読み込んだinit.elのあるディレクトリへ変更する
+    (setq user-emacs-directory (file-name-directory load-file-name)))
+
+  (unless (equal user-emacs-directory
+                 (expand-file-name "~/.emacs.d/"))
+    (cl-pushnew user-emacs-directory load-path)))
+
+(setup-user-emacs-directory)
 
 (cl-pushnew (locate-user-emacs-file "init.d")
             load-path)
 (require 'init-load)
 (put 'downcase-region 'disabled nil)
-
-;;;; gc
-(setq gc-cons-threshold (* 8 1024 1024))
