c2507af2ef19721c2d43e57d6049be053cc32d95
mytoh <mytoh@gmail.com>
2015-06-21 00:23:45 +0900

    use web-mode instead of builtin css-mode

--- init.d/layer/package/web-mode/init.el
+++ init.d/layer/package/web-mode/init.el
@@ -5,13 +5,14 @@
 ;;;;; web-mode
 (liby 'web-mode
   (command (web-mode) "web-mode")
-  (mode "\\.phtml\\'"  'web-mode)
-  (mode "\\.tpl\\.php\\'"  'web-mode)
-  (mode "\\.[gj]sp\\'"  'web-mode)
-  (mode "\\.as[cp]x\\'"  'web-mode)
-  (mode "\\.erb\\'"  'web-mode)
-  (mode "\\.mustache\\'"  'web-mode)
-  (mode "\\.djhtml\\'"  'web-mode)
-  (mode "\\.html?\\'"  'web-mode))
+  (mode "\\.phtml\\'"  #'web-mode)
+  (mode "\\.tpl\\.php\\'"  #'web-mode)
+  (mode "\\.[gj]sp\\'"  #'web-mode)
+  (mode "\\.as[cp]x\\'"  #'web-mode)
+  (mode "\\.erb\\'"  #'web-mode)
+  (mode "\\.mustache\\'"  #'web-mode)
+  (mode "\\.djhtml\\'"  #'web-mode)
+  (mode "\\.html?\\'"  #'web-mode)
+  (mode "\\.css\\'" #'web-mode))
 
 ;;; init.el ends here
