e0113c5e0a875f6b34bc1da8d096edfed20cdbd0
mytoh <mytoh@gmail.com>
2015-06-24 00:39:57 +0900

    update key bindings

--- init.d/layer/vim/org-mode/init.el
+++ init.d/layer/vim/org-mode/init.el
@@ -3,18 +3,24 @@
 ;;; Code:
 
 (evil-leader/set-key-for-mode 'org-mode
-                              "mc" #'org-capture
-                              "md" #'org-deadline
-                              "me" #'org-export-dispatch
-                              "mf" #'org-set-effort
-                              "mi" #'org-clock-in
-                              "mj" #'helm-org-in-buffer-headings
-                              "mo" #'org-clock-out
-                              "mm" #'org-ctrl-c-ctrl-c
-                              "mn" #'org-narrow-to-subtree
-                              "mN" #'widen
-                              "mq" #'org-clock-cancel
-                              "mr" #'org-refile
-                              "ms" #'org-schedule)
+    "m'" #'org-edit-special
+    "mc" #'org-capture
+    "md" #'org-deadline
+    "me" #'org-export-dispatch
+    "mf" #'org-set-effort
+    ;; headings
+    "mhi" #'org-insert-heading-after-current
+    "mhI" #'org-insert-heading
+    "mI" #'org-clock-in
+    "m," #'org-ctrl-c-ctrl-c
+    "mn" #'org-narrow-to-subtree
+    "mN" #'widen
+    "mO" #'org-clock-out
+    "mq" #'org-clock-cancel
+    "mR" #'org-refile
+    "ms" #'org-schedule
+    ;; insertion of common elements
+    "mil" #'org-insert-link
+    "mif" #'org-footnote-new)
 
 ;;; init.el ends here
