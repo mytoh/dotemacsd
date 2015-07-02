7490271014b1a960ca47ec3c48c8826ceee17587
mytoh <mytoh@gmail.com>
2015-07-02 11:46:27 +0900

    check library availability

--- init.d/layer/vim/expand-region/init.el
+++ init.d/layer/vim/expand-region/init.el
@@ -3,9 +3,9 @@
 ;;; Code:
 
 ;;;; [[http://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity/]]
-(after 'expand-region
-    (evil-global-set-key 'visual "v"
-     #'er/expand-region)
+(liby 'expand-region
+  (evil-global-set-key 'visual "v"
+                       #'er/expand-region)
   (evil-global-set-key 'visual (kbd "C-v")
                        #'er/contract-region))
 
