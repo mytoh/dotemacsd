4427c7a0dc3c66534bd5d342e5d0e4ee3a327b92
mytoh <mytoh@gmail.com>
2015-07-22 00:16:57 +0900

    use new macro

--- init.d/layer/vim/emacs-lisp-mode/init.el
+++ init.d/layer/vim/emacs-lisp-mode/init.el
@@ -2,19 +2,22 @@
 
 ;;; Code:
 
+(require 'muki-evil)
+
 ;; from spacemacs github.com/syl20bnr/spacemacs/spacemacs/keybindings.el
 ;; emacs-lisp -----------------------------------------------------------------
-(evil-leader/set-key-for-mode 'emacs-lisp-mode
-    "me$" #'lisp-state-eval-sexp-end-of-line
-    "meb" #'eval-buffer
-    "mee" #'eval-last-sexp
-    "mep" #'pp-eval-last-sexp
-    "mef" #'eval-defun
-    "mel" #'lisp-state-eval-sexp-end-of-line
-    "m,"  #'lisp-state-toggle-lisp-state)
+(muki:evil-leader-define-key-for-mode (emacs-lisp-mode emacs-lisp-mode-map)
+  "e$" #'lisp-state-eval-sexp-end-of-line
+  "eb" #'eval-buffer
+  "ee" #'eval-last-sexp
+  "ep" #'pp-eval-last-sexp
+  "ef" #'eval-defun
+  "er" 'eval-region
+  "el" #'lisp-state-eval-sexp-end-of-line
+  ","  #'lisp-state-toggle-lisp-state
 
-(evil-leader/set-key-for-mode 'emacs-lisp-mode
-    "mgg" 'elisp-slime-nav-find-elisp-thing-at-point
-    "mhh" 'elisp-slime-nav-describe-elisp-thing-at-point)
+  "gg" 'elisp-slime-nav-find-elisp-thing-at-point
+  "hh" 'elisp-slime-nav-describe-elisp-thing-at-point)
+
 
 ;;; init.el ends here
