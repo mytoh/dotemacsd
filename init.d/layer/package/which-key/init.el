934d2c53cf714c54712ca1c420addd90dba2c222
mytoh <mytoh@gmail.com>
2015-07-25 07:21:17 +0900

    which-key: update setting

--- init.d/layer/package/which-key/init.el
+++ init.d/layer/package/which-key/init.el
@@ -4,6 +4,13 @@
 
 (liby 'which-key
   (req 'popwin)
-  (command (which-key-mode) "which-key"))
+  (command (which-key-mode) "which-key")
+  (setq which-key-max-description-length 32)
+  (which-key-mode)
+  (which-key-add-key-based-replacements
+   "SPC ?" "show keybindings"
+   "SPC u" "universal arg"
+   "SPC m" "major mode cmds"
+   (concat "SPC " ";") "M-x"))
 
 ;;; init.el ends here
