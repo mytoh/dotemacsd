7486447d8dbbe7a65d916621287fb45a0290154f
mytoh <mytoh@gmail.com>
2015-08-04 20:20:07 +0900

    lazy loding

--- init.d/layer/package/fill-column-indicator/init.el
+++ init.d/layer/package/fill-column-indicator/init.el
@@ -2,7 +2,8 @@
 
 ;;; Code:
 
-(req 'fill-column-indicator
+(liby 'fill-column-indicator
+  (command (fci-mode) "fill-column-indicator")
   (add-hook 'prog-mode-hook #'fci-mode)
   ;; (add-hook 'text-mode-hook #'fci-mode)
   )
