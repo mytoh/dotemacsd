f4577dad28d0d8320e334fa42812578af12e56b0
mytoh <mytoh@gmail.com>
2015-08-04 20:14:45 +0900

    use defery

--- init.d/layer/package/auto-save-buffers-enhanced/init.el
+++ init.d/layer/package/auto-save-buffers-enhanced/init.el
@@ -2,15 +2,17 @@
 
 ;;; Code:
 
-(req 'auto-save-buffers-enhanced
-  (set-option auto-save-buffers-enhanced-interval
-              20)
-  ;; exclude some files
-  (set-option auto-save-buffers-enhanced-exclude-regexps
-              '("COMMIT_EDITMSG\\'"
-                "^/ssh:" "^scp:" "/sudo:" "/multi:"))
-  ;; If you're using CVS or Subversion or git
-  (auto-save-buffers-enhanced-include-only-checkout-path t)
-  (auto-save-buffers-enhanced t))
+(liby 'auto-save-buffers-enhanced
+  (defery 'auto-save-buffers-enhanced)
+  (after 'auto-save-buffers-enhanced
+      (set-option auto-save-buffers-enhanced-interval
+       20)
+    ;; exclude some files
+    (set-option auto-save-buffers-enhanced-exclude-regexps
+                '("COMMIT_EDITMSG\\'"
+                  "^/ssh:" "^scp:" "/sudo:" "/multi:"))
+    ;; If you're using CVS or Subversion or git
+    (auto-save-buffers-enhanced-include-only-checkout-path t)
+    (auto-save-buffers-enhanced t)))
 
 ;;; init.el ends here
