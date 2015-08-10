9d37a4eda3b8701528ed78c1039f63c9e82a1376
mytoh <mytoh@gmail.com>
2015-08-10 23:28:46 +0900

    fix themes list

--- init.d/layer/package/theme-looper/init.el
+++ init.d/layer/package/theme-looper/init.el
@@ -6,7 +6,7 @@
   (defery 'theme-looper)
   (after 'theme-looper
       (theme-looper-set-theme-set
-       '(comidia planet monokai molokai subatomic256 firebelly
+       '(comidia planet monokai molokai firebelly
          gruvbox base16-tomorrow odersky jazz purple-haze
          warm-night brin gotham soothe spolsky wilson noctilux erosiond
          material subatomic ectoplasm spacemacs-dark
