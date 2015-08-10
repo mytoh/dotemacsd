e10052513480415d118ed45574a6539e7b0fd47e
mytoh <mytoh@gmail.com>
2015-08-10 23:37:47 +0900

    cleanup list

--- init.d/layer/package/psession/init.el
+++ init.d/layer/package/psession/init.el
@@ -4,8 +4,7 @@
 
 (req 'psession
   (set-option psession-object-to-save-alist
-              '((ioccur-history . "ioccur-history.el")
-                (extended-command-history . "extended-command-history.el")
+              '((extended-command-history . "extended-command-history.el")
                 (helm-external-command-history . "helm-external-command-history.el")
                 (helm-surfraw-engines-history . "helm-surfraw-engines-history.el")
                 (helm-ff-history . "helm-ff-history.el")
