2e21d4c132652e3103689daad71640aba1244eec
mytoh <mytoh@gmail.com>
2015-08-16 11:29:55 +0900

    disable auto-saving in insert mode

--- init.d/layer/package/auto-save-buffers-enhanced/init.el
+++ init.d/layer/package/auto-save-buffers-enhanced/init.el
@@ -13,6 +13,25 @@
                   "^/ssh:" "^scp:" "/sudo:" "/multi:"))
     ;; If you're using CVS or Subversion or git
     (auto-save-buffers-enhanced-include-only-checkout-path t)
-    (auto-save-buffers-enhanced t)))
+    (auto-save-buffers-enhanced t)
+
+    ;; evil integration
+    ;; disable auto-saving in insert mode
+    (cl-defun muki:auto-save-buffers-enhanced-disable ()
+      (when auto-save-buffers-enhanced-activity-flag
+        (setq auto-save-buffers-enhanced-activity-flag nil)
+        (message "auto-save-buffers-enhanced off")))
+
+    (cl-defun muki:auto-save-buffers-enhanced-enable ()
+      (unless auto-save-buffers-enhanced-activity-flag
+        (setq auto-save-buffers-enhanced-activity-flag t)
+        (message "auto-save-buffers-enhanced on")))
+
+    (add-hook 'evil-insert-state-entry-hook
+              #'muki:auto-save-buffers-enhanced-disable)
+
+    (add-hook 'evil-insert-state-exit-hook
+              #'muki:auto-save-buffers-enhanced-enable)
+    ))
 
 ;;; init.el ends here
