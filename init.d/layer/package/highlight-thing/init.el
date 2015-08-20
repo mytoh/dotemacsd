3ba4feaf20dc164ddf54b29c0f0d0299711ab821
mytoh <mytoh@gmail.com>
2015-08-20 23:29:42 +0900

    defer package loading

--- init.d/layer/package/highlight-thing/init.el
+++ init.d/layer/package/highlight-thing/init.el
@@ -2,7 +2,8 @@
 
 ;;; Code:
 
-(req 'highlight-thing
+(liby 'highlight-thing
+  (command (highlight-thing-mode) "highlight-thing")
   (add-hook 'emacs-lisp-mode-hook #'highlight-thing-mode))
 
 ;;; init.el ends here
