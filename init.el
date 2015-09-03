53bfd9c29058a9a34c2f2719325b6ea62d5ef6bc
mytoh <mytoh@gmail.com>
2015-09-04 04:43:48 +0900

    update

--- init.el
+++ init.el
@@ -1,24 +1,11 @@
 ;;; -*- coding: utf-8 -*-
 
-(require 'cl-lib)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+(package-initialize)
 
-(cl-defun setup-user-emacs-directory ()
-  ;; [[http://d.hatena.ne.jp/peccu/20130218/trial_init]]
-  ;; make emacs loadable like this
-  ;; $ emacs -q -l /path/to/init.el
-  ;;
-  ;; emacs -l init.el のように起動されるとload-file-nameにinit.elのパスが入るので
-  (when load-file-name
-    ;; 設定ファイルの基準となるディレクトリを読み込んだinit.elのあるディレクトリへ変更する
-    (setq user-emacs-directory (file-name-directory load-file-name)))
 
-  (unless (equal user-emacs-directory
-                 (expand-file-name "~/.emacs.d/"))
-    (cl-pushnew user-emacs-directory load-path)))
-
-(setup-user-emacs-directory)
-
-(cl-pushnew (locate-user-emacs-file "init.d")
-            load-path)
+(add-to-list 'load-path (concat user-emacs-directory "init.d"))
 (require 'init-load)
 (put 'downcase-region 'disabled nil)
