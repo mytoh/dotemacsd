f8ad194fe0016c9a30a912588da7248484f9d6e9
mytoh <mytoh@gmail.com>
2015-09-05 16:51:44 +0900

    add key bindings

--- init.d/layer/vim/eww-mode/init.el
+++ init.d/layer/vim/eww-mode/init.el
@@ -2,6 +2,8 @@
 
 ;;; Code:
 
+(require 'muki-evil)
+
 (evil-define-key 'normal eww-mode-map
   "C-i" #'shr-next-link
   "S-<TAB>" #'shr-previous-link
@@ -10,5 +12,10 @@
 
 (evil-define-key 'normal eww-link-keymap
   "o" #'ace-link-eww)
+
+(muki:evil-leader-define-key-for-mode (eww-mode eww-mode-map)
+  "hh" #'helm-eww-history
+  "hb" #'helm-eww-bookmark
+  "hs" #'helm-eww-search-engine)
 
 ;;; init.el ends here
