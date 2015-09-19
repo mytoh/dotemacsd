d6b28fb2244be5cd84f8deb627a633ffff828760
mytoh <mytoh@gmail.com>
2015-09-19 15:15:46 +0900

    add gnu and fix org package url

--- init.d/layer/elpa/archive/init.el
+++ init.d/layer/elpa/archive/init.el
@@ -4,7 +4,8 @@
 
 (req 'package
   ;; (setq package-archives '())
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") 'append)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") 'append)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") 'append)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") 'append)
   (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") 'append)
   (add-to-list 'package-archives '("SC" . "http://joseito.republika.pl/sunrise-commander/") 'append)
