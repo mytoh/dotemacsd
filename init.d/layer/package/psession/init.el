12b618a868d690f80dfacdb719e2c5ca1291fd68
mytoh <mytoh@gmail.com>
2015-10-14 05:27:21 +0900

    * init.d/layer/package/psession/init.el: update

--- init.d/layer/package/psession/init.el
+++ init.d/layer/package/psession/init.el
@@ -4,11 +4,13 @@
 
 (req 'psession
   (set-option psession-object-to-save-alist
-              '((extended-command-history . "extended-command-history.el")
-                (helm-external-command-history . "helm-external-command-history.el")
-                (helm-surfraw-engines-history . "helm-surfraw-engines-history.el")
-                (helm-ff-history . "helm-ff-history.el")
-                (helm-grep-history . "helm-grep-history.el")
+              `((extended-command-history . "extended-command-history.el")
+                ,@(if (locate-library "helm")
+                      '((helm-external-command-history . "helm-external-command-history.el")
+                        (helm-surfraw-engines-history . "helm-surfraw-engines-history.el")
+                        (helm-ff-history . "helm-ff-history.el")
+                        (helm-grep-history . "helm-grep-history.el")
+                        (helm-M-x-input-history . "helm-M-x-input-history")))
                 (kill-ring . "kill-ring.el")
                 (kill-ring-yank-pointer . "kill-ring-yank-pointer.el")
                 (register-alist . "register-alist.el")
@@ -18,6 +20,12 @@
                 (evil-ex-search-history . "evil-ex-search-history.el")
                 (evil-search-forward-history . "evil-search-forward-history.el")
                 (evil-search-backward-history . "evil-search-backward-history.el")))
+  (hook 'psession-mode-hook
+     (lambda ()
+       (remove-hook 'kill-emacs-hook 'psession--dump-some-buffers-to-list)
+       (remove-hook 'emacs-startup-hook 'psession--restore-some-buffers)
+       (remove-hook 'kill-emacs-hook 'psession-save-last-winconf)
+       (remove-hook 'emacs-startup-hook 'psession-restore-last-winconf)))
   (enable-mode psession-mode))
 
 ;;; init.el ends here
