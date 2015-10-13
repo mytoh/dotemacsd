8fa8b08b0426a99a065e7be5c9ca5d8d7386ac0a
mytoh <mytoh@gmail.com>
2015-10-13 14:29:39 +0900

    * init.d/layer/package/nameless/init.el: fix function name

--- init.d/layer/package/nameless/init.el
+++ init.d/layer/package/nameless/init.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 (req 'nameless
-  (command (nameless-mode) "nameless")
-  ;; (hook 'emacs-lisp-mode-hook #'nameless-mode)
+  (command (nameless-mode nameless-mode-from-hook) "nameless")
+  ;; (add-hook 'emacs-lisp-mode-hook #'nameless-mode-from-hook)
   (after 'nameless
       (set-option nameless-prefix "^:")))
 
