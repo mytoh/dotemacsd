009cf8c3a4b3f3d0b49a096f3ef012a8085fcdf5
mytoh <mytoh@gmail.com>
2015-10-13 14:29:15 +0900

    * init.d/layer/package/info-plus/init.el: fix

--- init.d/layer/package/info-plus/init.el
+++ init.d/layer/package/info-plus/init.el
@@ -3,10 +3,10 @@
 ;;; Code:
 
 (after "info"
-    (req 'info+)
+    (req 'info+
   (set-face-attribute 'info-function-ref-item nil
                       :background "#222222")
   (set-face-attribute 'info-reference-item nil
-                      :background "#333333"))
+                      :background "#333333")))
 
 ;;; init.el ends here
