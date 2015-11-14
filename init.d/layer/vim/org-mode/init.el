2c95ce290c818778938d57ac588ba93df303a51a
mytoh <mytoh@gmail.com>
2015-11-15 04:53:55 +0900

    * init.d/layer/vim/org-mode/init.el: add new function

--- init.d/layer/vim/org-mode/init.el
+++ init.d/layer/vim/org-mode/init.el
@@ -4,6 +4,12 @@
 
 (require 'muki-evil)
 
+(cl-defun muki:org-insert-heading ()
+  (interactive)
+  (org-insert-heading)
+  (when (bound-and-true-p evil-mode)
+    (evil-insert-state)))
+
 (muki:evil-leader-define-key-for-mode (org-mode org-mode-map)
   "'" #'org-edit-special
   "c" #'org-capture
@@ -12,7 +18,7 @@
   "f" #'org-set-effort
   ;; headings
   "hi" #'org-insert-heading-after-current
-  "hI" #'org-insert-heading
+  "hI" #'muki:org-insert-heading
   "I" #'org-clock-in
   "," #'org-ctrl-c-ctrl-c
   "n" #'org-narrow-to-subtree
