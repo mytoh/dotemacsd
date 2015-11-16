4643ee58b4f3cf004fae76155ec118e8404a5ef3
mytoh <mytoh@gmail.com>
2015-11-17 08:11:26 +0900

    * init.d/layer/elpa/archive/init.el: use https

--- init.d/layer/elpa/archive/init.el
+++ init.d/layer/elpa/archive/init.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 (req 'package
-  ;; (setq package-archives '())
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") 'append)
+  (setq package-archives '())
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") 'append)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") 'append)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") 'append)
   (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") 'append)
