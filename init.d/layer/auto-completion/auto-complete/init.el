d2c9fb78f4015b1309d5947ab22630cc38a98186
mytoh <mytoh@gmail.com>
2015-12-05 19:17:14 +0900

    * auto-completion/auto-complete/init.el: change keymap

--- init.d/layer/auto-completion/auto-complete/init.el
+++ init.d/layer/auto-completion/auto-complete/init.el
@@ -6,6 +6,10 @@
 
 (req 'auto-complete-config
   (add-to-list 'ac-dictionary-directories "~/.emacs.d/hoarder/github.com/auto-complete/auto-complete/dict")
-  (ac-config-default))
+  (ac-config-default)
+  (setq ac-use-menu-map t)
+  ;; Default settings
+  (define-key ac-menu-map "\C-n" 'ac-next)
+  (define-key ac-menu-map "\C-p" 'ac-previous))
 
 ;;; init.el ends here
