6ec1c7cd4f915b63d333f5e7225026d9ad679d72
mytoh <mytoh@gmail.com>
2015-12-16 18:06:39 +0900

    * package/highlight-leading-spaces: fix

--- init.d/layer/package/highlight-leading-spaces/init.el
+++ init.d/layer/package/highlight-leading-spaces/init.el
@@ -6,7 +6,7 @@
 
 (command (highlight-leading-spaces-mode) "highlight-leading-spaces")
 (add-hook 'emacs-lisp-mode-hook #'highlight-leading-spaces-mode)
-(after 'highlight-leading-spaces-mode
+(after 'highlight-leading-spaces
     (set-face-foreground 'highlight-leading-spaces
      (color-lighten-name (face-background 'default) 12)))
 
