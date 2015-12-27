1a6dd205d4ac0fa327132533d7b2a58fbef0a055
mytoh <mytoh@gmail.com>
2015-12-28 05:52:15 +0900

    * package/highlight-thing: change color

--- init.d/layer/package/highlight-thing/init.el
+++ init.d/layer/package/highlight-thing/init.el
@@ -4,6 +4,11 @@
 
 (liby 'highlight-thing
   (command (highlight-thing-mode) "highlight-thing")
-  (add-hook 'emacs-lisp-mode-hook #'highlight-thing-mode))
+  (add-hook 'emacs-lisp-mode-hook #'highlight-thing-mode)
+  (after 'highlight-thing
+      (set-face-foreground 'highlight-thing
+       (face-foreground 'default))
+    (set-face-background 'highlight-thing
+                         (color-lighten-name (face-background 'default) 20))))
 
 ;;; init.el ends here
