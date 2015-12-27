96af7813a34c7e6f4839861bf49186287f4a1b0c
mytoh <mytoh@gmail.com>
2015-12-28 05:49:00 +0900

    * auto-completion/auto-complete: update

--- init.d/layer/auto-completion/auto-complete/init.el
+++ init.d/layer/auto-completion/auto-complete/init.el
@@ -6,11 +6,19 @@
 
 (req 'auto-complete-config
   (add-to-list 'ac-dictionary-directories "~/.emacs.d/hoarder/github.com/auto-complete/auto-complete/dict")
-  (ac-config-default)
-  (setq ac-use-menu-map t)
-  (setq ac-dwim t)
-  ;; Default settings
-  (define-key ac-menu-map "\C-n" 'ac-next)
-  (define-key ac-menu-map "\C-p" 'ac-previous))
+  (setq ac-auto-start 0
+        ac-delay 0.2
+        ac-quick-help-delay 1.
+        ac-use-fuzzy t
+        ac-fuzzy-enable t
+        ac-dwim t)
+  (setq-default ac-sources '(ac-source-abbrev
+                             ac-source-dictionary
+                             ac-source-words-in-same-mode-buffers))
+
+  (add-to-list 'completion-styles 'initials t)
+  (define-key ac-completing-map (kbd "C-j") #'ac-next)
+  (define-key ac-completing-map (kbd "C-k") #'ac-previous)
+  (define-key ac-completing-map (kbd "<S-tab>") #'ac-previous))
 
 ;;; init.el ends here
