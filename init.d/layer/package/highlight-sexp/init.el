48938f30d53c78c4234c999ff6b560a9c3d6a534
mytoh <mytoh@gmail.com>
2015-12-28 05:51:18 +0900

    * package/highlight-sexp: change color

--- init.d/layer/package/highlight-sexp/init.el
+++ init.d/layer/package/highlight-sexp/init.el
@@ -8,6 +8,8 @@
   (add-hook 'emacs-lisp-mode-hook #'highlight-sexp-mode)
   (add-hook 'scheme-mode-hook #'highlight-sexp-mode)
   (set-option hl-sexp-foreground-color nil)
-  (set-option hl-sexp-background-color "#1a1a1a"))
+  (set-option hl-sexp-background-color
+              (color-darken-name (face-background 'default)
+                                 40)))
 
 ;;; init.el ends here
