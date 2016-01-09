703ec6c2067439d0d9178f976fcd11d39f8b7f45
mytoh <mytoh@gmail.com>
2016-01-10 02:03:03 +0900

    * layer/auto-completion/auto-complete: enable ac-mode

--- init.d/layer/auto-completion/auto-complete/init.el
+++ init.d/layer/auto-completion/auto-complete/init.el
@@ -22,11 +22,12 @@
   (define-key ac-completing-map (kbd "<S-tab>") #'ac-previous)
 
   (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  (add-hook 'lisp-interaction-mode-hook 'ac-emacs-lisp-mode-setup)
   (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
   (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
   (add-hook 'css-mode-hook 'ac-css-mode-setup)
   (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-  ;; (global-auto-complete-mode t)
+  (global-auto-complete-mode t)
   )
 
 ;;; init.el ends here
