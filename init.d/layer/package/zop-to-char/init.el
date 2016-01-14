332df79baf7206f122acd9e8d474de26eb86a10e
mytoh <mytoh@gmail.com>
2016-01-14 22:48:23 +0900

    * layer/package/zop-to-char: add setting

---
+++ init.d/layer/package/zop-to-char/init.el
@@ -0,0 +1,10 @@
+;;; init --- init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(liby 'zop-to-char
+  (command '(zop-to-char zop-up-to-char)  "zop-to-char.el"))
+
+;;; init.el ends here
