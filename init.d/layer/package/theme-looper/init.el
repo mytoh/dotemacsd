929cc1a5c537e6d1591aa840260f3b9f0b161cb5
mytoh <mytoh@gmail.com>
2016-01-20 21:10:39 +0900

    * layer/package/theme-looper: add theme

--- init.d/layer/package/theme-looper/init.el
+++ init.d/layer/package/theme-looper/init.el
@@ -12,6 +12,6 @@
          material subatomic ectoplasm spacemacs-dark
          base16-ocean-dark zerodark plan9 spacegray atom-one-dark
          aurora arjen-grey dracula inkpot idea-darkula
-         reykjavik morning-star majapahit-dark))))
+         reykjavik morning-star majapahit-dark redbelly))))
 
 ;;; init.el ends here
