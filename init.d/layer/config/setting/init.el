c8163600d54f781e2309609df3131bb1e56dda42
mytoh <mytoh@gmail.com>
2016-02-01 19:02:54 +0900

    * layer/config/setting: disable electric-indent

--- init.d/layer/config/setting/init.el
+++ init.d/layer/config/setting/init.el
@@ -207,7 +207,7 @@
 (disable-option electric-layout-mode)
 
 ;;;; electric indent
-(disable-option electric-indent-mode)
+;; (disable-option electric-indent-mode)
 
 ;;;; add newline to end of file
 (enable-option require-final-newline)
