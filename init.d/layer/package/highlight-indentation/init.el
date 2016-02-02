95003de0ef451a3563da68227b1954ce63c299e3
mytoh <mytoh@gmail.com>
2016-02-03 03:09:35 +0900

    * layer/package/highlight-indentation: change faces

--- init.d/layer/package/highlight-indentation/init.el
+++ init.d/layer/package/highlight-indentation/init.el
@@ -6,8 +6,12 @@
   (command (highlight-indentation-current-column-mode)
            "highlight-indentation")
   (after 'highlight-indentation
-      (set-face-background 'highlight-indentation-face "#e3e3d3")
-    (set-face-background 'highlight-indentation-current-column-face "#c3b3b3"))
+      (set-face-background 'highlight-indentation-face
+       (color-lighten-name (face-background 'default)
+                           25))
+    (set-face-background 'highlight-indentation-current-column-face
+                         (color-lighten-name (face-background 'default)
+                                             25)))
   (add-hook 'emacs-lisp-mode-hook #'highlight-indentation-current-column-mode))
 
 ;;; init.el ends here
