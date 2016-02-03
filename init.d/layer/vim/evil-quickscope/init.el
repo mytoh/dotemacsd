79de4a2ca27b6ccdc16730fce4d96dd58fb780f9
mytoh <mytoh@gmail.com>
2016-02-04 08:01:01 +0900

    * layer/vim/evil-quickscope: update

--- init.d/layer/vim/evil-quickscope/init.el
+++ init.d/layer/vim/evil-quickscope/init.el
@@ -3,7 +3,9 @@
 ;;; Code:
 
 (liby 'evil-quickscope
-  (command (evil-quickscope-mode) "evil-quickscope")
-  (add-hook 'emacs-lisp-mode-hook #'evil-quickscope-mode))
+  (command (turn-on-evil-quickscope-always-mode
+            evil-quickscope-mode)
+           "evil-quickscope")
+  (add-hook 'emacs-lisp-mode-hook 'turn-on-evil-quickscope-always-mode))
 
 ;;; init.el ends here
