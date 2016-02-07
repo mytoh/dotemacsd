2f9a9fb980c043d239a550c461efa127d0f5ee51
mytoh <mytoh@gmail.com>
2016-02-07 09:23:57 +0900

    * layer/package/rainbow-mode: enable on help-mode

--- init.d/layer/package/rainbow-mode/init.el
+++ init.d/layer/package/rainbow-mode/init.el
@@ -10,6 +10,7 @@
   (hook 'css-mode-hook #'rainbow-mode)
   (hook 'js3-mode-hook #'rainbow-mode)
   (hook 'js-mode-hook #'rainbow-mode)
+  (hook 'help-mode-hook #'rainbow-mode)
   (after 'diminish
       (after 'rainbow-mode
           (diminish 'rainbow-mode " ω"))))
