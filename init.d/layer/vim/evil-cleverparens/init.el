409a94457939b747704434b1420bf5cb92b9fd24
mytoh <mytoh@gmail.com>
2016-03-10 20:43:32 +0900

    * vim/evil-cleverparens: fix for evil-snipe

--- init.d/layer/vim/evil-cleverparens/init.el
+++ init.d/layer/vim/evil-cleverparens/init.el
@@ -5,6 +5,14 @@
 (liby 'evil-cleverparens
   (command (evil-cleverparens-mode) "evil-cleverparens")
   (enable-option evil-cleverparens-use-regular-insert)
+  (after 'evil-cleverparens
+      (after 'evil-snipe
+          (setq evil-cp-regular-bindings
+           (seq-remove (lambda (p)
+                         (pcase (car p)
+                           ((or "s" "S")
+                            t)))
+                       evil-cp-regular-bindings))))
   (add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode))
 
 ;;; init.el ends here
