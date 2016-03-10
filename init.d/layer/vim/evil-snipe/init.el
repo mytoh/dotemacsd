4833b788e100e65e9b7ba11c8a21b2e6bcffdc3f
mytoh <mytoh@gmail.com>
2016-03-10 20:44:01 +0900

    * vim/evil-snipe: fix

--- init.d/layer/vim/evil-snipe/init.el
+++ init.d/layer/vim/evil-snipe/init.el
@@ -4,9 +4,9 @@
 
 (cl-defun muki:init-evil-snipe ()
   (require 'evil-snipe)
-  (add-hook 'prog-mode-hook #'evil-snipe-mode)
-  (add-hook 'prog-mode-hook
-            (lambda () (enable-mode evil-snipe-override-mode))))
+  (add-hook 'emacs-lisp-mode-hook #'evil-snipe-local-mode)
+  (add-hook 'emacs-lisp-mode-hook
+            #'evil-snipe-override-local-mode))
 
 (muki:init-evil-snipe)
 
