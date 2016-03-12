77276315b7a854ea7b4b9794673436d8e9acc0a2
mytoh <mytoh@gmail.com>
2016-03-12 19:21:05 +0900

    * init.d/layer/package-manager/register/init.el: add

--- init.d/layer/package-manager/register/init.el
+++ init.d/layer/package-manager/register/init.el
@@ -2098,6 +2098,12 @@
   '(:tag ["rice" "desktop"]))
 (hoarder:fetch "uncomplicate/fluokitten"
   '(:tag ["clojure" "haskell" "monad" "category"]))
+(hoarder:fetch "purnam/brahmin"
+  '(:tag ["clojure" "haskell" "monad" "category"]))
+(hoarder:fetch "eosrei/emojione-color-font"
+  '(:tag ["font" "emoji" "emojione"]))
+(hoarder:fetch "ramda/ramda"
+  '(:tag ["javascript"]))
 
 ;; https://gist.github.com/p1ch-jp/2912dc157b53449f7d1b
 
