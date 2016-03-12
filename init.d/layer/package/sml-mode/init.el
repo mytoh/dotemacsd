3fc082aff038a6351c96594eebae45e08c8c50d9
mytoh <mytoh@gmail.com>
2016-03-13 04:26:57 +0900

    * init.d/layer/package/sml-mode/init.el: fix

--- init.d/layer/package/sml-mode/init.el
+++ init.d/layer/package/sml-mode/init.el
@@ -5,6 +5,7 @@
 (liby 'sml-mode
   (command (sml-mode) "sml-mode")
   (mode "\\.s\\(ml\\|ig\\)\\'" #'sml-mode)
-  (mode "\\.\\([Mm][Ll]\\|fun\\|mlb\\)\\'" #'sml-mode))
+  ;; (mode "\\.\\(fun\\|mlb\\)\\'" #'sml-mode)
+  )
 
 ;;; init.el ends here
