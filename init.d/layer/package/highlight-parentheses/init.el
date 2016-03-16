faf4e5c1a3aff60a100d33757020f3ae8adfa432
mytoh <mytoh@gmail.com>
2016-03-16 23:26:14 +0900

    * layer/package/highlight-parentheses: change background color

--- init.d/layer/package/highlight-parentheses/init.el
+++ init.d/layer/package/highlight-parentheses/init.el
@@ -6,6 +6,8 @@
   (command (highlight-parentheses-mode) "highlight-parentheses")
   (add-hook 'prog-mode-hook #'highlight-parentheses-mode)
   (set-option hl-paren-delay 0.2)
+  (set-option hl-paren-background-colors
+              '("Lightyellow4"))
   )
 
 ;;; init.el ends here
