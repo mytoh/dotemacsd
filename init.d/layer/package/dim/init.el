e8a99b3f4a0344f91418760da24af6d4c03ef501
mytoh <mytoh@gmail.com>
2016-03-19 10:07:57 +0900

    * init.d/layer/package/dim/init.el: update

--- init.d/layer/package/dim/init.el
+++ init.d/layer/package/dim/init.el
@@ -5,19 +5,16 @@
 ;;; Code:
 
 (req 'dim
-  (dim-major-name 'scheme-mode "λ")
-  (dim-major-name 'help-mode "🄷")
   (dim-minor-name 'isearch-mode " 🔎")
   (dim-minor-name 'view-mode " 👀" 'view)
   (dim-major-names
-   '((emacs-lisp-mode           "EL")
-     (emacs-lisp-byte-code-mode "EL-byte")
-     (calendar-mode             "📆")))
+   '((calendar-mode             "📆")))
   (dim-minor-names
    '((visual-line-mode   " ↩")
      (auto-fill-function " ↵")
      (eldoc-mode         ""    eldoc)
      (whitespace-mode    " _"  whitespace)
-     (paredit-mode       " ()" paredit))))
+     (paredit-mode       " ()" paredit)
+     (helm-mode          " ⎈" helm))))
 
 ;;; init.el ends here
