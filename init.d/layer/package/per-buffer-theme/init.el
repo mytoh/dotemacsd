699002ca97ed36570e4ee9d131b2836fb38aec4d
mytoh <mytoh@gmail.com>
2016-03-22 03:27:01 +0900

    * package/per-buffer-theme: change themes

--- init.d/layer/package/per-buffer-theme/init.el
+++ init.d/layer/package/per-buffer-theme/init.el
@@ -10,10 +10,10 @@
       (set-option per-buffer-theme/default-theme
        muki:x-theme)
     (set-option per-buffer-theme/themes-alist
-                '(((:theme . notheme)
+                '(((:theme . base16-oceanicnext-dark)
                    (:buffernames "*eww" "*w3m" "*mu4e")
                    (:modes eww-mode w3m-mode cfw:calendar-mode mu4e-main-mode mu4e-headers-mode mu4e-view-mode mu4e-compose-mode mu4e-about-mode mu4e-update-mode))
-                  ((:theme . base16-greenscreen-dark)
+                  ((:theme . matrix)
                    (:buffernames "*mesh")
                    (:modes eshell-mode))))
     (per-buffer-theme/enable)))
