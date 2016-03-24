9a0c9a13b41dead932741a60b5b1f3e5872da7f9
mytoh <mytoh@gmail.com>
2016-03-24 21:27:04 +0900

    * package/hl-line: add tweak for eshell

--- init.d/layer/package/hl-line/init.el
+++ init.d/layer/package/hl-line/init.el
@@ -22,16 +22,17 @@
 
   (require 'hl-line)
   (defun global-hl-line-timer-function ()
-    (global-hl-line-unhighlight-all)
-    (let ((global-hl-line-mode t))
-      (global-hl-line-highlight)))
+    (unless (eq major-mode 'eshell-mode)
+      (global-hl-line-unhighlight-all)
+      (let ((global-hl-line-mode t))
+        (global-hl-line-highlight))))
   (setq global-hl-line-timer
         (run-with-idle-timer 0.03 t 'global-hl-line-timer-function))
   ;; (cancel-timer global-hl-line-timer)
 
 
-  (add-hook 'prog-mode-hook #'hl-line-mode)
-  (add-hook 'org-mode-hook #'hl-line-mode)
+  ;; (add-hook 'prog-mode-hook #'hl-line-mode)
+  ;; (add-hook 'org-mode-hook #'hl-line-mode)
   )
 
 ;;; init.el ends here
