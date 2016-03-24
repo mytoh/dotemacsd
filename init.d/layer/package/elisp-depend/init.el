77c9a2dc4833f30be8ce49f5252ca6cc44fe2867
mytoh <mytoh@gmail.com>
2016-03-25 05:04:57 +0900

    * init.d/layer/package/elisp-depend/init.el: update

--- init.d/layer/package/elisp-depend/init.el
+++ init.d/layer/package/elisp-depend/init.el
@@ -8,6 +8,22 @@
 
 (req 'elisp-depend
   (command (elisp-depend-insert-require)
-           "elisp-depend"))
+           "elisp-depend")
+  (set-option elisp-depend-directory-list
+              `(,(file-name-as-directory
+                  (muki:expand-path-huone "ohjelmat/emacs/share/emacs"))
+                 ,(file-name-as-directory
+                   (muki:expand-path-huone "ateljee"))))
+  (colle:each
+   (lambda (d)
+     (cl-pushnew d elisp-depend-special-explorers))
+   `((cl-defmacro elisp-depend-defun-form->sym-list)
+     (cl-defun elisp-depend-defun-form->sym-list)
+     (cl-letf elisp-depend-let-form->sym-list)
+     (cl-letf* elisp-depend-let-form->sym-list)
+     (req ,(lambda (sexp)
+             (elisp-depend-get-syms-recurse sexp 2)))))
+  )
+
 
 ;;; init.el ends here
