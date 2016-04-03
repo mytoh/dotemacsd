68f1b5a5de058d9bd7cf8610756e0d2e65477704
mytoh <mytoh@gmail.com>
2016-04-03 22:26:54 +0900

    * init.d/layer/elpa/archive/init.el: use non-HTTPS

--- init.d/layer/elpa/archive/init.el
+++ init.d/layer/elpa/archive/init.el
@@ -6,7 +6,7 @@
   (setq package-archives '())
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") 'append)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") 'append)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") 'append)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") 'append)
   ;; (add-to-list 'package-archives '("SC" . "http://joseito.republika.pl/sunrise-commander/") 'append)
   )
 
