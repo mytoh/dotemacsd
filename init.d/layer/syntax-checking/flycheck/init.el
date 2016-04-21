20b9c9c405f4fd667e816acbb6228fe969299dfd
mytoh <mytoh@gmail.com>
2016-04-22 00:25:33 +0900

    * layer/syntax-checking/flycheck: remove unused code

--- init.d/layer/syntax-checking/flycheck/init.el
+++ init.d/layer/syntax-checking/flycheck/init.el
@@ -5,18 +5,6 @@
   ;; (add-hook 'emacs-startup-hook #'global-flycheck-mode)
   (set-option flycheck-check-syntax-automatically '(save new-line))
 
-  ;; (flycheck-define-checker javascript-jslint-reporter
-  ;;   "A JavaScript syntax and style checker based on JSLint Reporter.
-
-  ;;                             See URL `https://github.com/FND/jslint-reporter'."
-  ;;   :command ("~/.emacs.d/jslint-reporter" source)
-  ;;   :error-patterns
-  ;;   ((error line-start (1+ nonl) ":" line ":" column ":" (message) line-end))
-  ;;   :modes (js-mode js2-mode js3-mode))
-  ;; (add-hook 'js-mode-hook (clambda ()
-  ;;                           (flycheck-select-checker 'javascript-jslint-reporter)
-  ;;                           (flycheck-mode)))
-
   (setq flycheck-emacs-lisp-load-path 'inherit)
 
   (add-hook 'flycheck-mode-hook
