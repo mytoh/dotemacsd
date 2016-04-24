0fd2d93eae202b9183b7f48f9303b0dd8bd1681d
mytoh <mytoh@gmail.com>
2016-04-24 17:10:32 +0900

    * init.d/layer/vim/general/init.el: update

--- init.d/layer/vim/general/init.el
+++ init.d/layer/vim/general/init.el
@@ -6,31 +6,37 @@
 
 (req 'general
 
-  (general-evil-setup)
+  (setq general-define-keymaps 'evil-normal-state-map)
+  (setq general-default-prefix "SPC")
+  (setq general-default-states '(normal motion visual))
+  
+  (general-define-key "ff" 'find-file
+                      "bb" 'switch-to-buffer
+                      "bk" 'kill-buffer
+                      "bl" 'list-buffers
+                      "fw" 'save-buffer)
 
-  (defvar muki:leader-key "SPC")
+  (general-define-key 
+   ;; :keymaps 'mesh-mode-map
+   "zz" #'mesh:switch)
 
-  (general-nmap :prefix muki:leader-key
-                ;; :keymaps 'mesh-mode-map
-                "zz" #'mesh:switch)
-
-  (general-nmap :prefix muki:leader-key
-                :keymaps 'eshell-mode-map
-                "z z" #'mesh:switch
-                "z c" #'mesh:new-tab
-                "z n" #'mesh:next-tab
-                "z p" #'mesh:prev-tab
-                "z s" #'mesh:split-tab
-                "z S" #'mesh:vsplit-tab
-                "z x" #'mesh:kill-pane
-                "z )" #'mesh:next-session
-                "z (" #'mesh:prev-session
-                "z C" #'mesh:new-session
-                "z <tab>" #'mesh:next-pane)
+  (general-define-key
+   :keymaps 'eshell-mode-map
+   "z z" #'mesh:switch
+   "z c" #'mesh:new-tab
+   "z n" #'mesh:next-tab
+   "z p" #'mesh:prev-tab
+   "z s" #'mesh:split-tab
+   "z S" #'mesh:vsplit-tab
+   "z x" #'mesh:kill-pane
+   "z )" #'mesh:next-session
+   "z (" #'mesh:prev-session
+   "z C" #'mesh:new-session
+   "z <tab>" #'mesh:next-pane)
 
   ;; all keywords arguments are still supported
-  (general-nmap :prefix muki:leader-key
-                "ap" #'helm-alku)
+  (general-define-key
+   "ap" #'helm-alku)
   )
 
 ;;; init.el ends here
