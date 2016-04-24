147d0b9cc06fed46487c4a5df4a00fbe9d7b4d00
mytoh <mytoh@gmail.com>
2016-04-24 12:23:36 +0900

    * layer/helm/ace-jump-helm-line: update

--- init.d/layer/helm/ace-jump-helm-line/init.el
+++ init.d/layer/helm/ace-jump-helm-line/init.el
@@ -6,8 +6,9 @@
   (command (ace-jump-helm-line) "ace-jump-helm-line")
   (after 'helm
       (define-key helm-map (kbd "C-'") #'ace-jump-helm-line))
+  ;; [[http://rubikitch.com/2015/04/16/ace-jump-helm-line/]]
   (after 'ace-jump-helm-line
-      (setq avi-keys (append "asdfghjklzxcvbnmqwertyuiop" nil))))
+      (setq avi-keys (append "aoeuidhtn;qjkxbm',.pyfgcrl" nil))))
 
 
 ;;; init.el ends here
