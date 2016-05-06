6274acc7ffe844f0063a928d982ae44ffd2939cb
mytoh <mytoh@gmail.com>
2016-05-06 23:38:02 +0900

    * init.d/lib/init-lib.el: fix load-path

--- init.d/lib/init-lib.el
+++ init.d/lib/init-lib.el
@@ -1,5 +1,8 @@
 
-(add-to-list 'load-path (locate-user-emacs-file "init.d/lib"))
+;; (add-to-list 'load-path (locate-user-emacs-file "init.d/lib"))
+(add-to-list 'load-path (expand-file-name
+                         "huone/ateljee/emacs-muki"
+                         (getenv "HOME")))
 (require 'muki)
 
 (provide 'init-lib)
