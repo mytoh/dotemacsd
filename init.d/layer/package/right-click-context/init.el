9c8ec92f16343da03c73bd219aebf16d3234bbb9
mytoh <mytoh@gmail.com>
2016-05-19 01:02:16 +0900

    * package/right-click-context: add seijiseikana functions

--- init.d/layer/package/right-click-context/init.el
+++ init.d/layer/package/right-click-context/init.el
@@ -8,6 +8,18 @@
   ;; Use emoji
   (setq right-click-context-mode-lighter "🐭")
 
+  (after 'seijiseikana
+      (setq right-click-context-global-menu-tree
+       (append right-click-context-global-menu-tree
+               (list
+                '("Seijiseikana"
+                  ("To Seiji"
+                   :call
+                   (seijiseikana-seiji-region beg end))
+                  ("To Ryakuji"
+                   :call
+                   (seijiseikana-ryakuji-region  beg end)))))))
+
   (right-click-context-mode 1))
 
 ;;; init.el ends here
