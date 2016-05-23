0ee81ba1b3c1d9dd09b37fa9e14a259ef404a486
mytoh <mytoh@gmail.com>
2016-05-23 22:05:39 +0900

    * package/highlight-tail: reenable settings for faces

--- init.d/layer/package/highlight-tail/init.el
+++ init.d/layer/package/highlight-tail/init.el
@@ -9,14 +9,14 @@
 
   ;; https://gist.github.com/jasonm23/f4e97fd68fe88c9c1acc
   ;; Green flame
-  ;; (setq  highlight-tail-colors '(("#000000" . 0)
-  ;;                                ("#00FF00" . 1)
-  ;;                                ("#006600" . 5)
-  ;;                                ("#002F00" . 15)
-  ;;                                ("#002C00" . 20)
-  ;;                                ("#001500" . 25)
-  ;;                                ("#001F00" . 30)
-  ;;                                ("#000000" . 35)))
+  (setq  highlght-tail-colors '(("#000000" . 0)
+                                ("#00FF00" . 1)
+                                ("#006600" . 5)
+                                ("#002F00" . 15)
+                                ("#002C00" . 20)
+                                ("#001500" . 25)
+                                ("#001F00" . 30)
+                                ("#000000" . 35)))
   (add-hook 'emacs-lisp-mode-hook #'highlight-tail-mode))
 
 
