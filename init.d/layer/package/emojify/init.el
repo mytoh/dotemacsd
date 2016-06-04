e6a0513e31d6db8a5c29290acf0b55add23f58a8
mytoh <mytoh@gmail.com>
2016-06-04 09:50:27 +0900

    * package/emojify: tweak emoji styles

--- init.d/layer/package/emojify/init.el
+++ init.d/layer/package/emojify/init.el
@@ -6,10 +6,13 @@
 
 (liby 'emojify
   (command (emojify-mode global-emojify-mode) "emojify")
-  (set-option emojify-emoji-styles '(unicode github)) ; ascii, github, unicode
+  (set-option emojify-emoji-styles '(unicode)) ; ascii, github, unicode
   (enable-mode global-emojify-mode)
   ;; (hook 'emacs-lisp-mode-hook #'emojify-mode)
-  ;; (hook 'magit-log-mode-hook #'emojify-mode)
+  (hook 'magit-log-mode-hook #'emojify-mode)
+  (hook 'magit-log-mode-hook
+        (lambda ()
+          (setq-local emojify-emoji-styles '(unicode github))))
   )
 
 ;;; :smile:
