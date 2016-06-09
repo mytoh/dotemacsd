e29822cca3a7f13591aaf0e08c28236604518541
mytoh <mytoh@gmail.com>
2016-06-10 06:13:27 +0900

    * package/theme-looper: make new function to filter themes

--- init.d/layer/package/theme-looper/init.el
+++ init.d/layer/package/theme-looper/init.el
@@ -7,20 +7,23 @@
 (liby 'theme-looper
   (defery 'theme-looper)
   (after 'theme-looper
-      (theme-looper-set-theme-set
-       (colle:filter
-        (lambda (theme)
-          (colle:find (lambda (tm) (eq tm theme))
-                   (custom-available-themes)))
-        '(comidia planet monokai molokai firebelly
-          gruvbox base16-tomorrow odersky jazz purple-haze
-          warm-night brin gotham soothe spolsky wilson noctilux erosiond
-          material subatomic ectoplasm spacemacs-dark
-          base16-ocean-dark zerodark plan9 spacegray atom-one-dark
-          aurora arjen-grey dracula inkpot idea-darkula
-          reykjavik morning-star majapahit-dark redbelly badwolf
-          hamburg omtose-phellack base16-harmonic16-dark
-          Motoko motoko-night base16-oceanicnext-dark
-          hyperplane iceberg myth)))))
+      (cl-defun muki:theme-looper-filter-available-themes (themes)
+        (colle:filter
+         (lambda (theme)
+           (colle:find (lambda (tm) (eq tm theme))
+                    (custom-available-themes)))
+         themes))
+    (theme-looper-set-theme-set
+     (muki:theme-looper-filter-available-themes
+      '(comidia planet monokai molokai firebelly
+        gruvbox base16-tomorrow odersky jazz purple-haze
+        warm-night brin gotham soothe spolsky wilson noctilux erosiond
+        material subatomic ectoplasm spacemacs-dark
+        base16-ocean-dark zerodark plan9 spacegray atom-one-dark
+        aurora arjen-grey dracula inkpot idea-darkula
+        reykjavik morning-star majapahit-dark redbelly badwolf
+        hamburg omtose-phellack base16-harmonic16-dark
+        Motoko motoko-night base16-oceanicnext-dark
+        hyperplane iceberg myth forest-blue)))))
 
 ;;; init.el ends here
