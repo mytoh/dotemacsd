9e16fe9e94497ce86d4add174ce29769b3ddb178
mytoh <mytoh@gmail.com>
2016-06-13 16:50:30 +0900

    * package/twittering-mode: update

--- init.d/layer/package/twittering-mode/init.el
+++ init.d/layer/package/twittering-mode/init.el
@@ -7,11 +7,20 @@
   (command (twit twittering-icon-mode) "twittering-mode")
   (setq twittering-use-master-password t)
   (setq twittering-allow-insecure-server-cert t)
-  (after 'twittering-mode
-      (twittering-icon-mode))
+  (setq twittering-icon-mode t)
+  (setq twittering-use-icon-storage t)
+  (setq twittering-convert-fix-size 52)
+  (setq twittering-display-remaining t)
+  (setq twittering-timeline-header  "")
+  (setq twittering-timeline-footer  "")
+  (setq twittering-status-format
+        "%i  %S, %RT{%FACE[bold]{%S}} %@  %FACE[shadow]{%p%f%L%r}\n%FOLD[        ]{%T}\n")
+  ;; (after 'twittering-mode
+  ;;     (twittering-icon-mode))
 
   (cl-defun muki:twittering-open-user (user)
     (interactive "sUser: ")
+    (twit)
     (twittering-visit-timeline `(user ,user)))
 
   )
