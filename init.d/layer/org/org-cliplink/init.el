ae86b5f4d64a56e4fe39ee817cc98976d5cb21b2
mytoh <mytoh@gmail.com>
2016-06-18 15:17:20 +0900

    * org/org-cliplink: use curl

--- init.d/layer/org/org-cliplink/init.el
+++ init.d/layer/org/org-cliplink/init.el
@@ -3,6 +3,8 @@
 ;;; Code:
 
 (liby 'org-cliplink
-  (command (org-cliplink) "org-cliplink"))
+  (command (org-cliplink) "org-cliplink")
+  (set-option org-cliplink-transport-implementation
+              'curl))
 
 ;;; init.el ends here
