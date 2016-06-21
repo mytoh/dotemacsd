8181fee9976be4010419379c8642765e18133e42
mytoh <mytoh@gmail.com>
2016-06-21 16:26:28 +0900

    * vim/evil-cleverparens: check smartparens

--- init.d/layer/vim/evil-cleverparens/init.el
+++ init.d/layer/vim/evil-cleverparens/init.el
@@ -4,17 +4,18 @@
 
 (req 'colle)
 
-(liby 'evil-cleverparens
-  (command (evil-cleverparens-mode) "evil-cleverparens")
-  (enable-option evil-cleverparens-use-regular-insert)
-  (after 'evil-cleverparens
-      (after 'evil-snipe
-          (setq evil-cp-regular-bindings
-           (colle:remove (lambda (p)
-                       (pcase (car p)
-                         ((or "s" "S")
-                          t)))
-                     evil-cp-regular-bindings))))
-  (add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode))
+(liby 'smartparens
+  (liby 'evil-cleverparens
+    (command (evil-cleverparens-mode) "evil-cleverparens")
+    (enable-option evil-cleverparens-use-regular-insert)
+    (after 'evil-cleverparens
+        (after 'evil-snipe
+            (setq evil-cp-regular-bindings
+             (colle:remove (lambda (p)
+                             (pcase (car p)
+                               ((or "s" "S")
+                                t)))
+                           evil-cp-regular-bindings))))
+    (add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)))
 
 ;;; init.el ends here
