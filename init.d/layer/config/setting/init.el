94ba4a0494df2aac67023f355696dd2ee4fccc5e
mytoh <mytoh@gmail.com>
2016-06-22 23:29:03 +0900

    * config/setting: add soria theme

--- init.d/layer/config/setting/init.el
+++ init.d/layer/config/setting/init.el
@@ -251,7 +251,7 @@
     reykjavik morning-star majapahit-dark redbelly badwolf
     hamburg omtose-phellack base16-harmonic16-dark
     Motoko motoko-night base16-oceanicnext-dark
-    hyperplane iceberg myth forest-blue))
+    hyperplane iceberg myth forest-blue soria))
 
 ;; (enable-option select-enable-clipboard
 ;;                select-enable-primary)
