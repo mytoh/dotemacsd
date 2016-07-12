0b84d317340b175f70b5e3cc118e497463e8f6d3
mytoh <mytoh@gmail.com>
2016-07-13 06:53:15 +0900

    * package/delight: add delight setting file

---
+++ init.d/layer/package/delight/init.el
@@ -0,0 +1,18 @@
+;;; init -- init -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(req 'delight
+  (delight 'which-key-mode " ⓘ" 'which-key)
+  (delight 'eldoc-mode " ⎎" 'eldoc)
+  (delight 'highlight-parentheses-mode " ❪❫" 'highlight-parentheses)
+  (delight 'highlight-symbol-mode " ✩" 'highlight-symbol)
+  (delight 'flycheck-mode " ✔" 'flycheck)
+  (delight 'outline-minor-mode " 📇" 'outline)
+  (delight 'golden-ratio-mode " ⊞" 'golden-ratio-mode)
+  (delight 'helm-mode " ⎈" 'helm)
+  (delight 'mesh-mode " 𐄳" 'mesh))
+
+;;; init.el ends here
