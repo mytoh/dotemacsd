020a24aa68fde5bc2d03ba5f8acf135c0b9837f7
mytoh <mytoh@gmail.com>
2016-07-15 18:30:32 +0900

    * package/paren: fix

--- init.d/layer/package/paren/init.el
+++ init.d/layer/package/paren/init.el
@@ -5,12 +5,12 @@
 ;;; Code:
 
 (add-hook 'prog-mode-hook #'show-paren-mode)
-(set-option show-paren-delay 0
+(set-option show-paren-delay 1
             show-paren-style 'parenthesis
             show-paren-when-point-inside-paren t
             show-paren-when-point-in-periphery t)
 (after 'paren
-    (set-face-attribute 'show-paren-match-face nil
+    (set-face-attribute 'show-paren-match nil
      :foreground nil
      :background (color-saturate-name
                   (face-background 'default)
