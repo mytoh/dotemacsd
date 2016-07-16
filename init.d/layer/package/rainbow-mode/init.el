a9402780b50daf2eeb32d1b7a78af3ffd82ec560
mytoh <mytoh@gmail.com>
2016-07-16 22:10:59 +0900

    * package/rainbow-mode: add settings

--- init.d/layer/package/rainbow-mode/init.el
+++ init.d/layer/package/rainbow-mode/init.el
@@ -3,6 +3,11 @@
 
 (liby 'rainbow-mode
   (command (rainbow-mode) "rainbow-mode")
+  (setq rainbow-html-colors t)
+  (setq rainbow-x-colors t)
+  (setq rainbow-latex-colors t)
+  (setq rainbow-ansi-colors t)
+  (hook 'html-mode-hook #'rainbow-mode)
   (hook 'emacs-lisp-mode-hook #'rainbow-mode)
   (hook 'lisp-mode-hook #'rainbow-mode)
   (hook 'scheme-mode-hook #'rainbow-mode)
