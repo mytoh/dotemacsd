587fcbd720583971d330a38eb491436504bdecfa
mytoh <mytoh@gmail.com>
2016-08-24 01:02:37 +0900

    * vim/evil-snipe: use req macro

--- init.d/layer/vim/evil-snipe/init.el
+++ init.d/layer/vim/evil-snipe/init.el
@@ -3,10 +3,10 @@
 ;;; Code:
 
 (cl-defun muki:init-evil-snipe ()
-  (require 'evil-snipe)
-  (add-hook 'emacs-lisp-mode-hook #'evil-snipe-local-mode)
-  (add-hook 'emacs-lisp-mode-hook
-            #'evil-snipe-override-local-mode))
+  (req 'evil-snipe
+    (add-hook 'emacs-lisp-mode-hook #'evil-snipe-local-mode)
+    (add-hook 'emacs-lisp-mode-hook
+              #'evil-snipe-override-local-mode)))
 
 (muki:init-evil-snipe)
 
