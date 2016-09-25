f1d79aa29f9eadbe65037926ab392ce45d804589
mytoh <mytoh@gmail.com>
2016-09-25 21:17:03 +0900

    * helm/helm-ag: add function from spacemacs

--- init.d/layer/helm/helm-ag/init.el
+++ init.d/layer/helm/helm-ag/init.el
@@ -8,7 +8,16 @@
 
   ;; (setq helm-ag-base-command
   ;;       "hw --no-group --no-color")
+  (defun muki:helm-project-do-ag ()
+    "Search in current project with `ag'.
+ref: [[https://github.com/syl20bnr/spacemacs/blob/develop/layers/+completion/helm/packages.el][spacemacs/packages.el at develop · syl20bnr/spacemacs · GitHub]] "
+    (interactive)
+    (let ((roots (project-roots (project-current))))
+      (if roots
+          (helm-do-ag (car roots))
+        (message "error: Not in a project."))))
   )
+
 
 ;; Local Variables:
 ;; coding: utf-8
