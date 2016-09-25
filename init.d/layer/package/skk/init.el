c65935e29de897dba9a6a11e59754750cfd232ee
mytoh <mytoh@gmail.com>
2016-09-25 21:17:54 +0900

    * package/skk: change keybind

--- init.d/layer/package/skk/init.el
+++ init.d/layer/package/skk/init.el
@@ -8,9 +8,8 @@
   ;; ~/.emacs.d/ddskk/init is setting file
   (set-option skk-user-directory (locate-user-emacs-file "ddskk/"))
   ;; C-x C-j で skk モードを起動
-  (add-global-key "C-x C-\\" #'skk-mode
-                  "C-x C-j" #'skk-mode
-                  )
+  (add-global-key "C-x j" #'skk-mode
+                  "C-x C-j" #'skk-mode)
   ;; enable skk mode by pressing カタカナ/ひらがな key
   (global-set-key [hiragana-katakana] #'skk-mode)
   ;; set default input method to skk
