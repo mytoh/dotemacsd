e9a1a49f021cb04607f8138a248d028974028f2f
mytoh <mytoh@gmail.com>
2017-09-28 16:33:48 +0900

    * package/hoarder: cleanup and add gitlab support

--- init.d/layer/package/hoarder/init.el
+++ init.d/layer/package/hoarder/init.el
@@ -44,34 +44,18 @@
                                         (seq-subseq url 19)
                                         (escape (decode-coding-string desc 'utf-8))
                                         (colle:map (lambda (topic) (concat "\"" topic "\"" )) topics))))))))))
-      (defun hoarder:github->fetch (url)
-        (interactive "sUrl: ")
+      (defun hoarder:insert-helper-github (command url)
         (request (concat "https://api.github.com/repos/"
                          (seq-subseq url 19))
                  :headers '(("Accept" . "application/vnd.github.mercy-preview+json"))
                  :parser 'json-read
                  :sync t
-                 :success (gen-callback "hoarder:fetch" url)))
-      (defun hoarder:github->register (url)
-        (interactive "sUrl: ")
-        (request (concat "https://api.github.com/repos/"
-                         (seq-subseq url 19))
-                 :headers '(("Accept" . "application/vnd.github.mercy-preview+json"))
-                 :parser 'json-read
-                 :sync t
-                 :success (gen-callback "hoarder:register" url)))
+                 :success (gen-callback (concat "hoarder:" command) url)))
 
       ;;  #; curl 'https://gitgud.io/api/v4/projects/nixx%2Fuguu'
       ;; {"id":3206,"description":"it's uguu.se but in Go","default_branch":"master","tag_list":[],"ssh_url_to_repo":"git@ssh.gitgud.io:nixx/uguu.git","http_url_to_repo":"https://gitgud.io/nixx/uguu.git","web_url":"https://gitgud.io/nixx/uguu","name":"uguu","name_with_namespace":"nixx / uguu","path":"uguu","path_with_namespace":"nixx/uguu","star_count":0,"forks_count":1,"created_at":"2016-12-20T11:18:11.452Z","last_activity_at":"2017-01-17T20:20:53.274Z"}~/.emacs.d/vendor/git.sv.gnu.org/emacs.git  emacs-26
-      (defun hoarder:gitgud->register (url)
-        (interactive "sUrl: ")
-        (request (concat "https://gitgud.io/api/v4/projects/"
-                         (url-hexify-string (pcase url ((and u
-                                                       (rx ".git"))
-                                                    (subseq (subseq u 18)
-                                                            0 -4))
-                                               (u
-                                                (subseq u 18)))))
+      (defun hoarder:insert-gitlab-internal (command repourl requrl)
+        (request requrl
                  :parser 'json-read
                  :sync t
                  :success
@@ -80,43 +64,53 @@
                     (cl-letf ((desc (assoc-default 'description data))
                               (taglist (assoc-default 'tag_list data)))
                       (insert
-                       (format "(hoarder:register \"%s\"
-'(:description \"%s\"
-:tags %s))\n"
-                               (pcase url ((and u
-                                          (rx ".git"))
-                                       u)
+                       (format "(hoarder:%s \"%s\"
+    '(:description \"%s\"
+      :tags %s))\n"
+                               command
+                               (pcase repourl ((and u
+                                              (rx ".git"))
+                                           u)
                                   (u
                                    (concat u ".git")))
                                desc
                                (colle:map (lambda (tag) (concat "\"" tag "\"")) taglist))))))))
-      (defun hoarder:gitgud->fetch (url)
+      (defun hoarder:insert-helper-gitlab (command url)
+        (hoarder:insert-gitlab-internal
+         command
+         url
+         (concat "https://gitlab.com/api/v4/projects/"
+                 (url-hexify-string
+                  (string-trim (subseq url 19) nil ".git")))))
+      (defun hoarder:insert-helper-gitgud (command url)
+        (hoarder:insert-gitlab-internal
+         command
+         url
+         (concat "https://gitgud.io/api/v4/projects/"
+                 (url-hexify-string (pcase url ((and u
+                                               (rx ".git"))
+                                            (subseq (subseq u 18)
+                                                    0 -4))
+                                       (u
+                                        (subseq u 18)))))))
+      (defun hoarder:insert-helper-register (url)
         (interactive "sUrl: ")
-        (request (concat "https://gitgud.io/api/v4/projects/"
-                         (url-hexify-string (pcase url ((and u
-                                                       (rx ".git"))
-                                                    (subseq (subseq u 18)
-                                                            0 -4))
-                                               (u
-                                                (subseq u 18)))))
-                 :parser 'json-read
-                 :sync t
-                 :success
-                 (cl-function
-                  (lambda (&key data &allow-other-keys)
-                    (cl-letf ((desc (assoc-default 'description data))
-                              (taglist (assoc-default 'tag_list data)))
-                      (insert
-                       (format "(hoarder:fetch \"%s\"
-'(:description \"%s\"
-:tags %s))\n"
-                               (pcase url ((and u
-                                          (rx ".git"))
-                                       u)
-                                  (u
-                                   (concat u ".git")))
-                               desc
-                               (colle:map (lambda (tag) (concat "\"" tag "\"")) taglist))))))))
+        (pcase url
+          ((rx "gitgud.io")
+           (hoarder:insert-helper-gitgud "register" url))
+          ((rx "github.com")
+           (hoarder:insert-helper-github "register" url))
+          ((rx "gitlab.com")
+           (hoarder:insert-helper-gitlab "register" url))))
+      (defun hoarder:insert-helper-fetch (url)
+        (interactive "sUrl: ")
+        (pcase url
+          ((rx "gitgud.io")
+           (hoarder:insert-helper-gitgud "fetch" url))
+          ((rx "github.com")
+           (hoarder:insert-helper-github "fetch" url))
+          ((rx "gitlab.com")
+           (hoarder:insert-helper-gitlab "fetch" url))))
       ))
   :config
   (progn
